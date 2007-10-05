8109606b5893dffaec518e9163a5a665ac30c8f0
prices <prices@b157844d-9f31-0410-aa6b-d326cbe640ab>
2007-10-05 18:09:26 +0000

    Moved to make the repository more understandable.
    git-svn-id: https://svn.hugllc.com/0039/trunk/HUGnetLib@313 b157844d-9f31-0410-aa6b-d326cbe640ab

--- lib/dbcheck.php
+++ lib/dbcheck.php
@@ -28,63 +28,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 	@date 04/12/04
 
 	
-	
-	$Log: dbcheck.php,v $
-	Revision 1.2  2005/11/23 15:19:40  prices
-	It should work more easily out of the box.
-	
-	Revision 1.1  2005/11/10 04:19:55  prices
-	periodic
-	
-	Revision 1.2  2005/06/10 14:42:02  prices
-	Added a check for a slave.
-	
-	Revision 1.1.1.1  2005/05/28 02:01:12  prices
-	Another restart
-	
-	
-	Revision 1.4  2005/05/23 15:14:27  prices
-	Changed the license to BSD.  All future releases will be under that license.
-	
-	Revision 1.3  2005/05/18 21:59:54  prices
-	Lots of changes and added files.
-	
-	Revision 1.1.1.1  2005/05/06 22:10:03  prices
-	Rewrite
-	
-	Revision 1.1  2005/05/06 13:32:48  prices
-	Checks the databases for problems and fixes them.
-	
-	
-
-	@verbatim
-	Copyright (c) 2005 Scott Price
-	All rights reserved.
-	Released under the BSD License:
-
-	Redistribution and use in source and binary forms, with or without modification, 
-	are permitted provided that the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright notice, 
-    this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright notice, 
-    this list of conditions and the following disclaimer in the documentation and/or 
-    other materials provided with the distribution.
-
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-	"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-	LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
-	FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
-	COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-	INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
-	BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
-	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
-	CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
-	LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
-	ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
-	POSSIBILITY OF SUCH DAMAGE.
-	@endverbatim
-
 
 	@cond SCRIPTS
 */
