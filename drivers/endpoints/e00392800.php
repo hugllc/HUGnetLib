e77afccd5219457b544be51fbc223dfd57d97dd1
prices <prices@b157844d-9f31-0410-aa6b-d326cbe640ab>
2007-12-18 20:00:35 +0000

    More doc improvements.
    git-svn-id: https://svn.hugllc.com/HUGnet/trunk/HUGnetLib@582 b157844d-9f31-0410-aa6b-d326cbe640ab

--- drivers/endpoints/e00392800.php
+++ drivers/endpoints/e00392800.php
@@ -39,14 +39,25 @@ if (!class_exists("e00392800")) {
 
     /**
      * Driver for the 0039-12 endpoint board and select firmwares
+     *
+     * @category   Drivers
+     * @package    HUGnetLib
+     * @subpackage Endpoints
+     * @author     Scott Price <prices@hugllc.com>
+     * @copyright  2007 Hunt Utilities Group, LLC
+     * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+     * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
      */
-    class e00392800 extends eDEFAULT{
-
+    class e00392800 extends eDEFAULT
+    {
+        /** The hardware name */
         var $HWName = "0039-28 Endpoint";
-
+        /** Average table to use */
         var $average_table = "e00392800_average";
+        /** History table to use */
         var $history_table = "e00392800_history";
 
+        /** Which devices are served by this class */
         var $devices = array(
             "0039-20-12-C" => array(
                 "0039-28-01-A" => "DEFAULT",
@@ -65,9 +76,10 @@ if (!class_exists("e00392800")) {
             ),
 
         );
-
+        /** Default locations of no others are specified */
         var $deflocation = array("Sensor 1", "Sensor 2", "Sensor 3", "Sensor 4", "Sensor 5", "Sensor 6", "Sensor 7", "Sensor 8", "Sensor 9");
 
+        /** Configurations supported */
         var $config = array(
             "0039-20-12-C" => array("Function" => "Pulse Counter", "Sensors" => 4),        
             "0039-20-13-C" => array("Function" => "Sensor Board", "Sensors" => 16),        
@@ -89,10 +101,15 @@ if (!class_exists("e00392800")) {
 
         /**
          * Returns the packet to send to read the configuration out of an endpoint
+         *
          * @param array $Info Infomation about the device to use
+         *
+         * @return array
+         *
          * @note This should only be defined in a driver that inherits this class if the packet differs
          */
-        function readConfig($Info) {
+        function readConfig($Info) 
+        {
             $packet = array(
                 array(
                     "To" => $Info["DeviceID"],
@@ -110,9 +127,16 @@ if (!class_exists("e00392800")) {
             return $packet;
         }
         /**
+         * Checks to make sure this is a valid record
          *
-          */
-        function checkRecord($Info, &$Rec) {
+         * @param array $Info Infomation about the device to use
+         * @param array &$Rec The record to check
+         *
+         * @return none
+         *
+         */
+        function checkRecord($Info, &$Rec) 
+        {
             parent::_checkRecordBase($Info, $Rec);    
             if ($Rec["Status"] == "BAD") return;
             if ($Rec["TimeConstant"] == 0) {
@@ -124,9 +148,15 @@ if (!class_exists("e00392800")) {
         }    
         
         /**
+         * Interprets the configuration
          *
-          */
-        function interpConfig(&$Info) {
+         * @param array $Info Infomation about the device to use
+         *
+         * @return none
+         *
+         */
+        function interpConfig(&$Info) 
+        {
             $this->_interpConfigDriverInfo($Info);
             $this->_interpConfigHW($Info);
             $this->_interpConfigFW($Info);
@@ -136,19 +166,33 @@ if (!class_exists("e00392800")) {
             $this->_interpTypes($Info);
             $this->_interpConfigSensorSetup($Info);
         }
+
         /**
+         * Adds config stuff specific to the 0039-20-12-C firmware
          *
-          */
-        private function _interpConfig00392012C(&$Info) {
+         * @param array $Info Infomation about the device to use
+         *
+         * @return none
+         *
+         */
+        private function _interpConfig00392012C(&$Info) 
+        {
             if ($Info["FWPartNum"] == "0039-20-12-C") {
                 $Info["Types"] = array(0 => 0x70, 1 => 0x70, 2 => 0x71, 3 => 0x72);
             }
         }
 
         /**
+         * Interprets sensor information
          *
-          */
-        function interpSensors($Info, $Packets) {
+         * @param array $Info    Infomation about the device to use
+         * @param array $Packets The sensor packets to interpret
+         *
+         * @return none
+         *
+         */
+        function interpSensors($Info, $Packets) 
+        {
             $this->interpConfig($Info);
             $ret = array();
 
@@ -164,13 +208,19 @@ if (!class_exists("e00392800")) {
                     $ret[] = $data;
                 }
             }
-            return($ret);
+            return $ret;
         }
 
         /**
+         * Gets the raw data from the sensor read
          *
-          */
-        private function _interpSensorsGetRaw(&$Info, &$data) {
+         * @param array $Info  Infomation about the device to use
+         * @param array &$data The data we are getting from the packets
+         *
+         * @return none
+         */
+        private function _interpSensorsGetRaw(&$Info, &$data) 
+        {
             if (is_array($data["Data"])) {
                 $index = 3;
                 for ($i = 0; $i < $Info["NumSensors"]; $i++) {
@@ -190,7 +240,8 @@ if (!class_exists("e00392800")) {
          *
          * @param object $driver Object of class driver.
         */    
-        function e00392800 (&$driver) {
+        function __construct(&$driver) 
+        {
             parent::__construct($driver);
         }
 
