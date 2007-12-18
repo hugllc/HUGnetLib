1e90aecf6d33f3bc5501ccb657cfb99302bc450b
prices <prices@b157844d-9f31-0410-aa6b-d326cbe640ab>
2007-12-18 18:27:46 +0000

    Moved this file
    git-svn-id: https://svn.hugllc.com/HUGnet/trunk/HUGnetLib@574 b157844d-9f31-0410-aa6b-d326cbe640ab

---
+++ database/Analysis.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Class for saving analysis data
+ *
+ * PHP Version 5
+ *
+ * <pre>
+ * HUGnetLib is a library of HUGnet code
+ * Copyright (C) 2007 Hunt Utilities Group, LLC
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   Database
+ * @package    HUGnetLib
+ * @subpackage Analysis
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2007 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    SVN: $Id$    
+ * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+
+/** The base for all database classes */
+require_once HUGNET_INCLUDE_PATH."/base/DbBase.php";
+
+/**
+ * A class for controlling processes
+ *
+ * @category   Database
+ * @package    HUGnetLib
+ * @subpackage Endpoints
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2007 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+class Analysis extends DbBase
+{
+    var $table = "Analysis";            //!< The database table to use
+    var $id = "AnalysisKey";     //!< This is the Field name for the key of the record
+   
+}
+
+?>
