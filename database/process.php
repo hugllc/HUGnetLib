c7ad409a32f9bb8f1f81cca5222b1c23f73b6bd9
prices <prices@b157844d-9f31-0410-aa6b-d326cbe640ab>
2007-12-25 15:41:05 +0000

    Changed the category
    git-svn-id: https://svn.hugllc.com/HUGnet/trunk/HUGnetLib@633 b157844d-9f31-0410-aa6b-d326cbe640ab

--- database/process.php
+++ database/process.php
@@ -23,7 +23,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  * </pre>
  *
- * @category   UnixProcess
+ * @category   Library
  * @package    HUGnetLib
  * @subpackage UnixProcess
  * @author     Scott Price <prices@hugllc.com>
