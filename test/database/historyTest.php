b91f5c4a9022872099634c2904578e1342a25540
prices <prices@b157844d-9f31-0410-aa6b-d326cbe640ab>
2008-02-05 19:09:44 +0000

    Keyword fixes
    git-svn-id: https://svn.hugllc.com/HUGnet/trunk/HUGnetLib@856 b157844d-9f31-0410-aa6b-d326cbe640ab

--- test/database/historyTest.php
+++ test/database/historyTest.php
@@ -29,7 +29,7 @@
  * @author     Scott Price <prices@hugllc.com>
  * @copyright  2007 Hunt Utilities Group, LLC
  * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
- * @version    SVN: $Id: AnalysisTest.php 590 2007-12-19 15:56:30Z prices $    
+ * @version    SVN: $Id$    
  * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
  */
 
