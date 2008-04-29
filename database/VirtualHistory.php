18486b69ccfb24f8640629f1ae375bf88937d1ea
prices <prices@b157844d-9f31-0410-aa6b-d326cbe640ab>
2008-04-29 20:55:48 +0000

    Worked on bug #819.  These are the initial files and tests.  I need to work on some other bugs to make testing this stuff possible.
    git-svn-id: https://svn.hugllc.com/HUGnet/trunk/HUGnetLib@1300 b157844d-9f31-0410-aa6b-d326cbe640ab

---
+++ database/VirtualHistory.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Class for saving analysis data
+ *
+ * PHP Version 5
+ *
+ * <pre>
+ * HUGnetLib is a library of HUGnet code
+ * Copyright (C) 2007 Hunt Utilities Group, LLC
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   Database
+ * @package    HUGnetLib
+ * @subpackage History
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2007 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    SVN: $Id$    
+ * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+
+/** The base for all database classes */
+require_once HUGNET_INCLUDE_PATH."/database/History.php";
+
+/**
+ * A class for controlling processes
+ *
+ * @category   Database
+ * @package    HUGnetLib
+ * @subpackage History
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2007 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+class VirtualHistory extends History
+{
+   
+    /**
+     * Gets all rows from the database
+     *
+     * @param string $where   Where clause
+     * @param array  $data    Data for query
+     * @param int    $limit   The maximum number of rows to return (0 to return all)
+     * @param int    $start   The row offset to start returning records at
+     * @param string $orderby How to order the string.  Must include "ORDER BY"
+     *
+     * @return array
+     */
+    public function getWhere($where, $data = array(), $limit = 0, $start = 0, $orderby="") 
+    {
+        
+    }
+
+    /**
+     * Creates the database table
+     *
+     * @param string $table    The table to use
+     * @param mixed  $elements The number of data fields
+     *
+     * @return null
+     */   
+    public function createTable($table=null, $elements=null)
+    {
+    }
+
+}
+
+?>
