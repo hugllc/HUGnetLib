65126fa701065b46f821d3dd22a84da4b58db764
Scott L. Price <prices@dflytech.com>
2009-01-07 16:03:07 -0600

    More work on bug #1519.  In this case it also included adding tests for voltageSensor::linearBounded and voltageSensor::linearBoundedIndirect.

--- test/drivers/sensors/voltageSensorTest.php
+++ test/drivers/sensors/voltageSensorTest.php
@@ -31,14 +31,9 @@
  * @copyright  2007-2009 Hunt Utilities Group, LLC
  * @copyright  2009 Scott Price
  * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
- * @version    SVN: $Id$    
+ * @version    SVN: $Id$
  * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
  */
-
-// Call voltageSensorTest::main() if this source file is executed directly.
-if (!defined("PHPUnit_MAIN_METHOD")) {
-    define("PHPUnit_MAIN_METHOD", "voltageSensorTest::main");
-}
 
 /** The test case class */
 require_once "PHPUnit/Framework/TestCase.php";
@@ -59,63 +54,66 @@ require_once dirname(__FILE__).'/../../../drivers/sensors/voltageSensor.php';
  * @copyright  2007-2009 Hunt Utilities Group, LLC
  * @copyright  2009 Scott Price
  * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
- * @version    SVN: $Id$    
  * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
  */
-class voltageSensorTest extends sensorTestBase
+class VoltageSensorTest extends sensorTestBase
 {
     var $class = "voltageSensor";
     /**
-     * Runs the test methods of this class.
-     *
-     * @return null
-     *
-     * @access public
-     * @static
-     */
+    * Runs the test methods of this class.
+    *
+    * @return null
+    *
+    * @access public
+    * @static
+    */
     public static function main()
     {
-        require_once "PHPUnit/TextUI/TestRunner.php";
+        include_once "PHPUnit/TextUI/TestRunner.php";
 
         $suite  = new PHPUnit_Framework_TestSuite("voltageSensorTest");
         $result = PHPUnit_TextUI_TestRunner::run($suite);
     }
 
     /**
-     * Sets up the fixture, for example, open a network connection.
-     * This method is called before a test is executed.
-     *
-     * @return null
-     *
-     * @access protected
-     */
+    * Sets up the fixture, for example, open a network connection.
+    * This method is called before a test is executed.
+    *
+    * @return null
+    *
+    * @access protected
+    */
     protected function setUp()
     {
     }
 
     /**
-     * Tears down the fixture, for example, close a network connection.
-     * This method is called after a test is executed.
-     *
-     * @return null
-     *
-     * @access protected
-     */
-    protected function tearDown() 
+    * Tears down the fixture, for example, close a network connection.
+    * This method is called after a test is executed.
+    *
+    * @return null
+    *
+    * @access protected
+    */
+    protected function tearDown()
     {
     }
     /**
-     * data provider for testSensorArray*
-     */    
-    public static function dataSensorArray() 
+    * data provider for testSensorArray*
+    *
+    * @return array
+    */
+    public static function dataSensorArray()
     {
         return sensorTestBase::sensorArrayDataSource("voltageSensor");
     }
 
     /**
-     * Data provider for testGetDividerVoltage
-     */
-    public static function dataGetDividerVoltage() 
+    * Data provider for testGetDividerVoltage
+    *
+    * @return array
+    */
+    public static function dataGetDividerVoltage()
     {
         return array(
             array(1, 1, 0, 1, 0.0),
@@ -124,73 +122,153 @@ class voltageSensorTest extends sensorTestBase
         );
     }
     /**
-     * test
-     *
-     * @return null
-     *
-     * @dataProvider dataGetDividerVoltage
-     * @covers voltageSensor::GetDividerVoltage
-     */
+    * test
+    *
+    * @param int   $A      The incoming value
+    * @param float $R1     The resistor to the voltage
+    * @param float $R2     The resistor to ground
+    * @param int   $T      The time constant
+    * @param mixed $expect The expected return value
+    *
+    * @return null
+    *
+    * @dataProvider dataGetDividerVoltage
+    * @covers voltageSensor::GetDividerVoltage
+    */
     public function testGetDividerVoltage($A, $R1, $R2, $T, $expect)
     {
-        $o = new voltageSensor();
+        $o   = new voltageSensor();
         $ret = $o->getDividerVoltage($A, $R1, $R2, $T);
         $this->assertSame($expect, $ret);
     }
 
     /**
-     * Data provider for testFETBoard
-     */
-    public static function dataFETBoard() 
+    * Data provider for testFETBoard
+    *
+    * @return array
+    */
+    public static function dataFETBoard()
     {
         return array(
-            array(1000, array("extraDefault"=>array(1, 1)), 1, array(1, 1), 0, 0.1527),
-            array(1000, array("extraDefault"=>array(1, 1)), 1, array(1, 0), 0, 0.1527),
-            array(1000, array("extraDefault"=>array(1, 1)), 1, array(0, 1), 0, 0.1527),
-            array(1000, array("extraDefault"=>array(1, 1)), 1, null, 0, 0.1527),
+            array(1000,
+                  array("extraDefault"=>array(1, 1)),
+                  1,
+                  array(1, 1),
+                  0,
+                  0.1527),
+            array(1000,
+                  array("extraDefault"=>array(1, 1)),
+                  1,
+                  array(1, 0),
+                  0,
+                  0.1527),
+            array(1000,
+                  array("extraDefault"=>array(1, 1)),
+                  1,
+                  array(0, 1),
+                  0,
+                  0.1527),
+            array(1000,
+                  array("extraDefault"=>array(1, 1)),
+                  1,
+                  null,
+                  0,
+                  0.1527),
         );
     }
     /**
-     * test
-     *
-     * @return null
-     *
-     * @dataProvider dataFETBoard
-     * @covers voltageSensor::FETBoard
-     */
-    public function testFETBoard($val, $sensor, $TC, $extra, $deltaT, $expect) 
+    * test
+    *
+    * @param float $val    The incoming value
+    * @param array $sensor The sensor setup array
+    * @param int   $TC     The time constant
+    * @param mixed $extra  Extra parameters for the sensor
+    * @param float $deltaT The time difference
+    * @param mixed $expect The expected return value
+    *
+    * @return null
+    *
+    * @dataProvider dataFETBoard
+    * @covers voltageSensor::FETBoard
+    */
+    public function testFETBoard($val, $sensor, $TC, $extra, $deltaT, $expect)
     {
-        parent::sensorTest("voltageSensor", "FETBoard", $val, $sensor, $TC, $extra, $deltaT, $expect);
+        parent::sensorTest("voltageSensor",
+                           "FETBoard",
+                           $val,
+                           $sensor,
+                           $TC,
+                           $extra,
+                           $deltaT,
+                           $expect);
     }
 
     /**
-     * Data provider for testFETBoard
-     */
-    public static function dataIndirect() 
+    * Data provider for testFETBoard
+    *
+    * @return array
+    */
+    public static function dataIndirect()
     {
         return array(
-            array(1000, array("extraDefault"=>array(1, 1, 5)), 1, array(1, 1, 5), 0, 0.1527),
-            array(1000, array("extraDefault"=>array(1, 1, 5)), 1, array(1, 0, 5), 0, 0.1527),
-            array(1000, array("extraDefault"=>array(1, 1, 5)), 1, array(0, 1, 5), 0, 0.1527),
-            array(1000, array("extraDefault"=>array(1, 1, 5)), 1, null, 0, 0.1527),
+            array(1000,
+                  array("extraDefault"=>array(1, 1, 5)),
+                  1,
+                  array(1, 1, 5),
+                  0,
+                  0.1527),
+            array(1000,
+                  array("extraDefault"=>array(1, 1, 5)),
+                  1,
+                  array(1, 0, 5),
+                  0,
+                  0.1527),
+            array(1000,
+                  array("extraDefault"=>array(1, 1, 5)),
+                  1,
+                  array(0, 1, 5),
+                  0,
+                  0.1527),
+            array(1000,
+                  array("extraDefault"=>array(1, 1, 5)),
+                  1,
+                  null,
+                  0,
+                  0.1527),
         );
     }
     /**
-     * test
-     *
-     * @return null
-     *
-     * @dataProvider dataIndirect
-     */
-    public function testIndirect($val, $sensor, $TC, $extra, $deltaT, $expect) 
+    * test
+    *
+    * @param float $val    The incoming value
+    * @param array $sensor The sensor setup array
+    * @param int   $TC     The time constant
+    * @param mixed $extra  Extra parameters for the sensor
+    * @param float $deltaT The time difference
+    * @param mixed $expect The expected return value
+    *
+    * @return null
+    *
+    * @dataProvider dataIndirect
+    */
+    public function testIndirect($val, $sensor, $TC, $extra, $deltaT, $expect)
     {
-        parent::sensorTest("voltageSensor", "Indirect", $val, $sensor, $TC, $extra, $deltaT, $expect);
+        parent::sensorTest("voltageSensor",
+                           "Indirect",
+                           $val,
+                           $sensor,
+                           $TC,
+                           $extra,
+                           $deltaT,
+                           $expect);
     }
 
     /**
-     * Data provider for GetVoltage
-     */
-    public static function dataGetVoltage() 
+    * Data provider for GetVoltage
+    *
+    * @return array
+    */
+    public static function dataGetVoltage()
     {
         return array(
             array(null, 1, 1, null),
@@ -200,23 +278,30 @@ class voltageSensorTest extends sensorTestBase
         );
     }
     /**
-     * test
-     *
-     * @return null
-     *
-     * @dataProvider dataGetVoltage
-     * @covers voltageSensor::GetVoltage
-     */
+    * test
+    *
+    * @param int   $A      The AtoD reading
+    * @param int   $T      The time constant
+    * @param float $Vref   The voltage reference
+    * @param mixed $expect The expected return value
+    *
+    * @return null
+    *
+    * @dataProvider dataGetVoltage
+    * @covers voltageSensor::GetVoltage
+    */
     public function testGetVoltage($A, $T, $Vref, $expect)
     {
-        $o = new voltageSensor();
+        $o   = new voltageSensor();
         $ret = $o->getVoltage($A, $T, $Vref);
         $this->assertSame($expect, $ret);
     }
 
     /**
-     * Data provider for testCHSMSS
-     */
+    * Data provider for testCHSMSS
+    *
+    * @return array
+    */
     public static function dataCHSMSS()
     {
         return array(
@@ -228,21 +313,37 @@ class voltageSensorTest extends sensorTestBase
         );
     }
     /**
-     * test
-     *
-     * @return null
-     *
-     * @dataProvider dataCHSMSS
-     * @covers voltageSensor::CHSMSS
-     */
-    public function testCHSMSS($val, $sensor, $TC, $extra, $deltaT, $expect) 
+    * test
+    *
+    * @param float $val    The incoming value
+    * @param array $sensor The sensor setup array
+    * @param int   $TC     The time constant
+    * @param mixed $extra  Extra parameters for the sensor
+    * @param float $deltaT The time difference
+    * @param mixed $expect The expected return value
+    *
+    * @return null
+    *
+    * @dataProvider dataCHSMSS
+    * @covers voltageSensor::CHSMSS
+    */
+    public function testCHSMSS($val, $sensor, $TC, $extra, $deltaT, $expect)
     {
-        parent::sensorTest("voltageSensor", "CHSMSS", $val, $sensor, $TC, $extra, $deltaT, $expect);
+        parent::sensorTest("voltageSensor",
+                           "CHSMSS",
+                           $val,
+                           $sensor,
+                           $TC,
+                           $extra,
+                           $deltaT,
+                           $expect);
     }
 
     /**
-     * Data provider for testCHSMSS
-     */
+    * Data provider for testCHSMSS
+    *
+    * @return array
+    */
     public static function dataDirect()
     {
         return array(
@@ -254,21 +355,136 @@ class voltageSensorTest extends sensorTestBase
         );
     }
     /**
-     * test
-     *
-     * @return null
-     *
-     * @dataProvider dataDirect
-     */
-    public function testDirect($val, $sensor, $TC, $extra, $deltaT, $expect) 
+    * test
+    *
+    * @param float $val    The incoming value
+    * @param array $sensor The sensor setup array
+    * @param int   $TC     The time constant
+    * @param mixed $extra  Extra parameters for the sensor
+    * @param float $deltaT The time difference
+    * @param mixed $expect The expected return value
+    *
+    * @return null
+    *
+    * @dataProvider dataDirect
+    */
+    public function testDirect($val, $sensor, $TC, $extra, $deltaT, $expect)
     {
-        parent::sensorTest("voltageSensor", "direct", $val, $sensor, $TC, $extra, $deltaT, $expect);
+        parent::sensorTest("voltageSensor",
+                           "direct",
+                           $val,
+                           $sensor,
+                           $TC,
+                           $extra,
+                           $deltaT,
+                           $expect);
+    }
+    /**
+    * Data provider for testLinearBounded
+    *
+    * @return array
+    */
+    public static function dataLinearBounded()
+    {
+        $extra  = array(.5, 4.5, 0, 5, 5);
+        $extra2 = array(5, 5, 0, 5, 5);
+        $extra3 = array();
+        $sensor = array('extraDefault' => array(1, 4, 0, 100, 5));
+        return array(
+            array(10000, $sensor, 1, $extra, 0, 0.3296),
+            array(null,  $sensor, 1, $extra, 0, null),
+            array(0, $sensor, 1, $extra, 0, null),
+            array(65535, $sensor, 1, $extra, 0, null),
+            array(10000, $sensor, 1, $extra2, 0, null),
+            array(30000, $sensor, 1, $extra3, 0, 43.0367),
+            array(null,  $sensor, 1, $extra3, 0, null),
+            array(0, $sensor, 1, $extra3, 0, null),
+            array(65535, $sensor, 1, $extra3, 0, null),
+            array(65536, $extra3, 1, $extra2, 0, null),
+
+        );
+    }
+    /**
+    * test
+    *
+    * @param float $val    The incoming value
+    * @param array $sensor The sensor setup array
+    * @param int   $TC     The time constant
+    * @param mixed $extra  Extra parameters for the sensor
+    * @param float $deltaT The time difference
+    * @param mixed $expect The expected return value
+    *
+    * @return null
+    *
+    * @dataProvider dataLinearBounded
+    */
+    public function testLinearBounded($val, $sensor, $TC, $extra, $deltaT, $expect)
+    {
+        parent::sensorTest("voltageSensor",
+                           "linearBounded",
+                           $val,
+                           $sensor,
+                           $TC,
+                           $extra,
+                           $deltaT,
+                           $expect);
     }
 
+     /**
+    * Data provider for testLinearBoundedIndirect
+    *
+    * @return array
+     */
+    public static function dataLinearBoundedIndirect()
+    {
+        $extra = array(100, 10, 5, 45, 0, 1000, 5);
+        $extra2 = array(100, 10, 25, 25, 0, 1000, 5);
+        $extra3 = array();
+        $sensor = array('extraDefault' => array(1000, 10, 5, 300, 0, 100, 5));
+        return array(
+            array(10000, $sensor, 1, $extra, 0, 85.0125),
+            array(null, $sensor, 1, $extra, 0, null),
+            array(0, $sensor, 1, $extra, 0, null),
+            array(65535, $sensor, 1, $extra, 0, null),
+            array(10000, $sensor, 1, $extra3, 0, 24.4516),
+            array(null, $sensor, 1, $extra3, 0, null),
+            array(0, $sensor, 1, $extra3, 0, null),
+            array(65535, $sensor, 1, $extra3, 0, null),
+
+        );
+    }
+    /**
+    * test
+    *
+    * @param float $val    The incoming value
+    * @param array $sensor The sensor setup array
+    * @param int   $TC     The time constant
+    * @param mixed $extra  Extra parameters for the sensor
+    * @param float $deltaT The time difference
+    * @param mixed $expect The expected return value
+    *
+    * @return null
+    *
+    * @dataProvider dataLinearBoundedIndirect
+    */
+    public function testLinearBoundedIndirect($val,
+                                              $sensor,
+                                              $TC,
+                                              $extra,
+                                              $deltaT,
+                                              $expect)
+    {
+        parent::sensorTest("voltageSensor",
+                           "linearBoundedIndirect",
+                           $val,
+                           $sensor,
+                           $TC,
+                           $extra,
+                           $deltaT,
+                           $expect);
+    }
+
+
 }
 
-// Call voltageSensorTest::main() if this source file is executed directly.
-if (PHPUnit_MAIN_METHOD == "voltageSensorTest::main") {
-    voltageSensorTest::main();
-}
 ?>
