a058d709ef5c8e3978efc5c374fbfcb2681bab5a
Scott L. Price <prices@dflytech.com>
2009-01-09 08:36:23 -0600

    Fixed more style errors.  I fixed bug #1519.  There are still style errors, but I can fix those on a different bug.  This bug only specified that I fix 350 errors, and I fixed almost 400.

--- drivers/socket/dbsocket.php
+++ drivers/socket/dbsocket.php
@@ -8,17 +8,17 @@
  * HUGnetLib is a library of HUGnet code
  * Copyright (C) 2007-2009 Hunt Utilities Group, LLC
  * Copyright (C) 2009 Scott Price
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -31,7 +31,7 @@
  * @copyright  2007-2009 Hunt Utilities Group, LLC
  * @copyright  2009 Scott Price
  * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
- * @version    SVN: $Id$    
+ * @version    SVN: $Id$
  * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
  */
 
@@ -40,22 +40,24 @@ require_once HUGNET_INCLUDE_PATH."/base/SocketBase.php";
 
 if (!class_exists("dbsocket")) {
     /**
-     * Class for talking with HUGNet endpoints through unix sockets
-     * 
-     * This class is meant to talk to something like {@link http://ser2net.sourceforge.net/ ser2net}
-     * or any of the serial to ethernet servers from {@link http://www.bb-elec.com/ B&B Electronics}.
-     * Other methods might work, but they are untested.  It basically expects a raw
-     * serial port that is accessable through a unix socket.  
-     *
-     * @category   Database
-     * @package    HUGnetLib
-     * @subpackage Socket
-     * @author     Scott Price <prices@hugllc.com>
-     * @copyright  2007-2009 Hunt Utilities Group, LLC
- * @copyright  2009 Scott Price
-     * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
-     * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
-     */
+    * Class for talking with HUGNet endpoints through unix sockets
+    *
+    * This class is meant to talk to something like
+    * {@link http://ser2net.sourceforge.net/ ser2net}
+    * or any of the serial to ethernet servers from
+    * {@link http://www.bb-elec.com/ B&B Electronics}.
+    * Other methods might work, but they are untested.  It basically expects a raw
+    * serial port that is accessable through a unix socket.
+    *
+    * @category   Database
+    * @package    HUGnetLib
+    * @subpackage Socket
+    * @author     Scott Price <prices@hugllc.com>
+    * @copyright  2007-2009 Hunt Utilities Group, LLC
+    * @copyright  2009 Scott Price
+    * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+    * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+    */
     class DbSocket extends SocketBase
     {
         /** @var string The database table to use */
@@ -74,39 +76,42 @@ if (!class_exists("dbsocket")) {
         var $Server = "";
         /** @var bool Whether this socket supports multiple packets */
         public $multiPacket = true;
-    
+
         /** @var int The timeout for waiting for a packet in seconds */
         var $ReplyTimeout = 20;
         /** @var bool Whether we should print out a lot output */
-        var $verbose = false;        
+        var $verbose = false;
         /** @var array Array of strings that we are reading */
-        var $readstr = array();        
-    
+        var $readstr = array();
+
         /** @var array The packet we are currently sending out */
         var $packet = array();
-    
+
         /** @var int The index of the reply array */
-        private $replyIndex = 0;
-        
-        private $index = 0;
+        protected $replyIndex = 0;
+        /** @var int The index */
+        protected $index = 0;
+        /** @var int The ID of the reply packet */
         protected $replyId = array();
         /**
-         * Write data out a socket.  
-         *
-         * Returns the random id on success, false on failure.
-         *
-         * @param string $data The data to send out the socket in string form
-         * @param array  $pkt  The data to send out the socket in array form
-         *
-         * @return mixed
-         */
-        public function write($pkt, $GetReply=true) 
+        * Write data out a socket.
+        *
+        * Returns the random id on success, false on failure.
+        *
+        * @param array $pkt      The data to send out the socket in array form
+        * @param bool  $GetReply Whether or not to get a reply
+        *
+        * @return mixed
+        */
+        public function write($pkt, $GetReply = true)
         {
-            $id                           = rand(1, 24777216);  // Big random number for the id
-            $pkt["PacketFrom"]            = $pkt["SentFrom"];
-            $this->packet[$id]            = $pkt;
-            $this->packet[$id]["id"]      = $id;
-            if ($GetReply) $this->replyId[$id] = time();
+            $id                      = rand(1, 24777216);  // Big random number
+            $pkt["PacketFrom"]       = $pkt["SentFrom"];
+            $this->packet[$id]       = $pkt;
+            $this->packet[$id]["id"] = $id;
+            if ($GetReply) {
+                $this->replyId[$id] = time();
+            }
             $ret = $this->socket->add($this->packet[$id]);
 
             if ($ret === false) {
@@ -116,66 +121,79 @@ if (!class_exists("dbsocket")) {
             }
         }
         /**
-         * Cleans up the replyID array
-         *
-         * @return null
-         */
+        * Cleans up the replyID array
+        *
+        * @return null
+        */
         protected function replyIdCleaner()
         {
             foreach ($this->replyId as $id => $time) {
-                if ($time < (time() - $this->ReplyTimeout)) unset($this->replyId[$id]);
+                if ($time < (time() - $this->ReplyTimeout)) {
+                    unset($this->replyId[$id]);
+                }
             }
         }
         /**
-         *  Gets the first of the packets that is destined for us.
-         *
-         * @return bool
-         */
-        private function _getPacket($reply=false) 
+        *  Gets the first of the packets that is destined for us.
+        *
+        * @param bool $reply Whether to expect a reply or not
+        *
+        * @return bool
+        */
+        private function _getPacket($reply = false)
         {
-            if ($reply) return $this->_getPacketReply();
+            if ($reply) {
+                return $this->_getPacketReply();
+            }
             static $lastCheck;
             $now = date("Y-m-d H:i:s");
-            
-            if (empty($lastCheck)) $lastCheck = "0000-00-00 00:00:00";
-            $query = " `Date` > ?";
+
+            if (empty($lastCheck)) {
+                $lastCheck = "0000-00-00 00:00:00";
+            }
+            $query  = " `Date` > ?";
             $data[] = $lastCheck;
-            $res = $this->socket->getWhere($query, $data);
-            $ret = false;
+            $res    = $this->socket->getWhere($query, $data);
+            $ret    = false;
             if (is_array($res)) {
                 foreach ($res as $pkt) {
                     $ret[] = $this->unbuildPacket($pkt);
                 }
             }
             // This causes a pause so we don't take all of the processing time
-            if ((count($res) == 0) || !is_array($res)) usleep(100000);
-            
+            if ((count($res) == 0) || !is_array($res)) {
+                usleep(100000);
+            }
             $lastCheck = $now;
             return $ret;
         }
         /**
-         *  Gets the first of the packets that is destined for us.
-         *
-         * @return bool
-         */
-        private function _getPacketReply() 
+        *  Gets the first of the packets that is destined for us.
+        *
+        * @return bool
+        */
+        private function _getPacketReply()
         {
             $query = " (Type = 'REPLY' ";
-            if ($this->getAll) $query .= " OR Type = 'UNSOLICITED'";
-            $query .= ")";
-            $data = array();
+            if ($this->getAll) {
+                $query .= " OR Type = 'UNSOLICITED'";
+            }
+            $query  .= ")";
+            $data    = array();
             $lastrun = $now;
             if (!empty($this->replyId) && !$this->getAll) {
-                $ids = array_keys($this->replyId);       
+                $ids    = array_keys($this->replyId);
                 $query .= " AND (id = ?";
-                $query .= str_repeat(" OR id = ?", count($ids) - 1); 
+                $query .= str_repeat(" OR id = ?", count($ids) - 1);
                 $query .= ")";
                 $query .= " AND (`Date` >= ?";
-                $query .= str_repeat(" OR `Date` >= ?", count($ids) - 1); 
+                $query .= str_repeat(" OR `Date` >= ?", count($ids) - 1);
                 $query .= ")";
 
-                $data  = array_merge($data, $ids);
-                foreach ($this->replyId as $time) $data[] = date("Y-m-d H:i:s", $time);
+                $data = array_merge($data, $ids);
+                foreach ($this->replyId as $time) {
+                    $data[] = date("Y-m-d H:i:s", $time);
+                }
             }
             $res = $this->socket->getWhere($query, $data);
             if (is_array($res)) {
@@ -189,130 +207,162 @@ if (!class_exists("dbsocket")) {
                 }
             }
             // This causes a pause so we don't take all of the processing time
-            if ((count($ret) == 0) || !is_array($ret)) usleep(300000);
+            if ((count($ret) == 0) || !is_array($ret)) {
+                usleep(300000);
+            }
             return false;
         }
-    
+
         /**
-         *  removes the packet for the id given
-         *
-         * @param int $id The random id of the packet to delete.
-         *
-         * @return null
-         */
-        private function _deletePacket($id) 
+        *  removes the packet for the id given
+        *
+        * @param int $id The random id of the packet to delete.
+        *
+        * @return null
+        */
+        private function _deletePacket($id)
         {
             unset($this->packet[$id]);
             $this->socket->remove($id);
-    
+
         }
         /**
-         * Receives a packet from the socket interface
-         *
-         * @param int $timeout Timeout for waiting.  Default is used if timeout == 0    
-         *
-         * @return bool false on failure, the Packet array on success
-         */
-        function RecvPacket($timeout=0, $reply = true) 
+        * Receives a packet from the socket interface
+        *
+        * @param int  $timeout Timeout for waiting.  Default is used if timeout == 0
+        * @param bool $reply   Whether to expect a reply or not
+        *
+        * @return bool false on failure, the Packet array on success
+        */
+        function recvPacket($timeout = 0, $reply = true)
         {
             $timeout  = $this->getReplyTimeout($timeout);
             $Start    = time();
             $GotReply = false;
-    
+
             do {
                 $GotReply = $this->_getPacket($reply);
-                if (is_array($GotReply)) break;
+                if (is_array($GotReply)) {
+                    break;
+                }
             } while ((time() - $Start) < $timeout);
             $this->replyIdCleaner();
             return $GotReply;
-    
+
         }
         /**
-         * Turns a packet string into an array of values.
-         *
-         * @param string $data The raw packet to parse
-         *
-         * @return array The packet array created from the string
-         */
+        * Turns a packet string into an array of values.
+        *
+        * @param string $data The raw packet to parse
+        *
+        * @return array The packet array created from the string
+        */
         function unbuildPacket($data)
         {
             // Strip off any preamble bytes.
             $pkt = array();
-            $pkt["Command"] = (empty($data["Command"])) ? $data["sendCommand"] : $data["Command"];
-            $pkt["To"]      = (empty($data["PacketTo"])) ? $data["To"] : $data["PacketTo"]; 
+
+            if (empty($data["Command"])) {
+                $pkt["Command"] = $data["sendCommand"];
+            } else {
+                $pkt["Command"] = $data["Command"];
+            }
+            if (empty($data["PacketTo"])) {
+                $pkt["To"] = $data["To"];
+            } else {
+                $pkt["To"] = $data["PacketTo"];
+            }
             devInfo::setStringSize($pkt["To"], 6);
-            $pkt["From"]    = (empty($data["PacketFrom"])) ? $data["From"] : $data["PacketFrom"];
+
+            if (empty($data["PacketFrom"])) {
+                $pkt["From"] = $data["From"];
+            } else {
+                $pkt["From"] = $data["PacketFrom"];
+            }
             devInfo::setStringSize($pkt["From"], 6);
-    
+
             $pkt["Length"]       = strlen($data["RawData"] / 2);
             $pkt["RawData"]      = $data["RawData"];
             $pkt["Data"]         = self::splitDataString($pkt["RawData"]);
             $pkt["Checksum"]     = self::PacketGetChecksum($pkt["RawData"]);
-            $pkt["CalcChecksum"]     = self::PacketGetChecksum($pkt["RawData"]);
+            $pkt["CalcChecksum"] = self::PacketGetChecksum($pkt["RawData"]);
             return $pkt;
         }
-        
+
         /**
-         * Closes the socket connection
-         * 
-         * @return null
-         */
-        public function close() 
+        * Closes the socket connection
+        *
+        * @return null
+        */
+        public function close()
         {
             $this->socket = false;
         }
-    
+
         /**
-         * Checks to make sure that all we are connected to the server
-         * 
-         * This routine only checks the connection.  It does nothing else.  If you want to
-         * have the script automatically connect if it is not connected already then use
-         * dbsocket::Connect().
-         *
-         * @return bool true if the connection is good, false otherwise
-         *
-         * @todo How do I make this check the connection when the connection might be
-         *      
-         */
-        public function checkConnect() 
+        * Checks to make sure that all we are connected to the server
+        *
+        * This routine only checks the connection.  It does nothing else.  If
+        * you want to have the script automatically connect if it is not
+        * connected already then use dbsocket::Connect().
+        *
+        * @return bool true if the connection is good, false otherwise
+        *
+        * @todo How do I make this check the connection when the connection might be
+        *
+        */
+        public function checkConnect()
         {
-            return is_object($this->socket) && (get_class($this->socket) == "HUGnetDB");
+            $test  = is_object($this->socket);
+            $test  = $test && (get_class($this->socket) == "HUGnetDB");
+            return $test;
         }
-        
+
         /**
-         * Connects to the server
-         * 
-         * This function first checks the connection.  If you are planning on checking the
-         * connection and want the server to automatically connect if not connectd, use this
-         * routine.  If you just want to check the connection, use ep_socket::CheckConnect.
-         *
-         *
-         * @return bool true if the connection is good, false otherwise
-         */
-        public function connect($config=array()) 
+        * Connects to the server
+        *
+        * This function first checks the connection.  If you are planning on
+        * checking the connection and want the server to automatically connect
+        * if not connectd, use this routine.  If you just want to check the
+        * connection, use ep_socket::CheckConnect.
+        *
+        * @param array $config The configuration array to use.
+        *
+        * @return bool true if the connection is good, false otherwise
+        */
+        public function connect($config = array())
         {
-            if (empty($config)) $config = $this->config;
-            if ($this->CheckConnect()) return true;
+            if (empty($config)) {
+                $config = $this->config;
+            }
+            if ($this->CheckConnect()) {
+                return true;
+            }
             $this->close();
             $this->socket = new HUGnetDB($config);
             return $this->CheckConnect();
-        }            
-    
-    
+        }
+
+
         /**
-         * Constructor
-         * 
-         * @param array $config The configuration to use.
-         *
-         * @return null
-         */
-        public function __construct($config = array()) 
+        * Constructor
+        *
+        * @param array $config The configuration to use.
+        *
+        * @return null
+        */
+        public function __construct($config = array())
         {
-            $config["table"] = empty($config["socketTable"]) ? "PacketLog" : $config["socketTable"];
+
+            if (empty($config["socketTable"])) {
+                $config["table"] = "PacketLog";
+            } else {
+                $config["table"] = $config["socketTable"];
+            }
             parent::__construct($config);
             $this->connect();
         }
-        
+
     }
 }
 ?>
