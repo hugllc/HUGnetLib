5f9eab865ec9d8579f46d506ab555e2590b7e362
Scott Price <prices@dflytech.com>
2009-02-02 08:23:58 -0600

    Modified

--- release.sh
+++ release.sh
@@ -3,13 +3,12 @@ GIT_SERVER=git://git.hugllc.com
 
 VERSION=$1
 
-if [ x${VERSION} == "x" ]; then
+if [ "x${VERSION}" == "x" ]; then
     echo "Usage:  ${0} <release version>"
     exit;
 fi
 
 git commit -a
 echo Tagging the version
-#svn -m "Release $VERSION" copy ./$COM_NAME ${SVN_SERVER}/HUGnet/tags/HUGnetLib/${VERSION}
-git tag -m "Release ${VERSION}" -s ${VERSION}
-git push
+git tag -m "Release ${VERSION}" -s v${VERSION} HEAD
+git push --tags
