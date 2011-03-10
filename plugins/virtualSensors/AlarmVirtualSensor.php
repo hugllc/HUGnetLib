9dfeb9f0b03a14a2ae19c510c266e2d6f1a5c840
Scott Price <prices@hugllc.com>
2011-03-10 09:52:23 -0600

    Fixed bug #4043.  This plugin only says whether it is in a alarm or not.  It doesn't actually raise an alarm anywhere.

---
+++ plugins/virtualSensors/AlarmVirtualSensor.php
@@ -0,0 +1,169 @@
+<?php
+/**
+ * This is the default endpoint driver and the base for all other
+ * endpoint drivers.
+ *
+ * PHP Version 5
+ * <pre>
+ * HUGnetLib is a library of HUGnet code
+ * Copyright (C) 2007-2011 Hunt Utilities Group, LLC
+ * Copyright (C) 2009 Scott Price
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   Misc
+ * @package    HUGnetLib
+ * @subpackage Endpoints
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2007-2011 Hunt Utilities Group, LLC
+ * @copyright  2009 Scott Price
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    SVN: $Id$
+ * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+/** This is for the base class */
+require_once dirname(__FILE__)."/../../base/VirtualSensorBase.php";
+
+/**
+ * This class has functions that relate to the manipulation of elements
+ * of the devInfo array.
+ *
+ * @category   Containers
+ * @package    HUGnetLib
+ * @subpackage Database
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2007-2011 Hunt Utilities Group, LLC
+ * @copyright  2009 Scott Price
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+class AlarmVirtualSensor extends VirtualSensorBase
+{
+    /** @var This is to register the class */
+    public static $registerPlugin = array(
+        "Name" => "Alarm Virtual Sensor",
+        "Type" => "sensor",
+        "Class" => "AlarmVirtualSensor",
+        "Flags" => array("FE:alarm"),
+    );
+    /** These are the endpoint information bits */
+    /** @var array This is the default values for the data */
+    protected $default = array(
+        "id" => 0xFE,                    // The id of the sensor.  This is the value
+                                         // Stored in the device  It will be an int
+        "type" => "alarm",                    // The type of the sensors
+        "location" => "",                // The location of the sensors
+        "dataType" => UnitsBase::TYPE_RAW,      // The datatype of each sensor
+        "extra" => array(),              // Extra input for crunching numbers
+        "units" => "",                   // The units to put the data into by default
+        "bound" => false,                // This says if this sensor is changeable
+        "rawCalibration" => "",          // The raw calibration string
+        "decimals" => null,
+    );
+    /** @var object These are the valid values for type */
+    protected $typeValues = array("alarm");
+    /**
+    * This is the array of sensor information.
+    */
+    protected $fixed = array(
+        "longName" => "Alarm Virtual Sensor",
+        "unitType" => "Percent",
+        // Integer is the size of the field needed to edit
+        // Array   is the values that the extra can take
+        // Null    nothing
+        "extraValues" => array(
+            5, 10, 10
+        ),
+        "extraText" => array(
+            "input", "Alarm Threshold", "Reset Threshold"
+        ),
+        "extraDefault" => array(
+            1, 1, 0
+        ),
+        "storageType" => UnitsBase::TYPE_RAW,  // This is the dataType as stored
+        "storageUnit" => "decimal",
+        "maxDecimals" => 4,
+    );
+
+    /**
+    * Disconnects from the database
+    *
+    * @param array  $data    The servers to use
+    * @param object &$device The device we are attached to
+    */
+    public function __construct($data, &$device)
+    {
+        $this->default["id"] = 0xFE;
+        $this->default["type"] = "alarm";
+        parent::__construct($data, $device);
+    }
+    /**
+    * Changes a raw reading into a output value
+    *
+    * @param int   $A      Output of the A to D converter
+    * @param float $deltaT The time delta in seconds between this record
+    * @param array &$data  The data from the other sensors that were crunched
+    * @param mixed $prev   The previous value for this sensor
+    *
+    * @return mixed The value in whatever the units are in the sensor
+    */
+    public function getReading($A, $deltaT = 0, &$data = array(), $prev = null)
+    {
+        $index = ((int)$this->getExtra(0)) - 1;
+        if (($index < 0)) {
+            return null;
+        }
+        $alarm = (float)$this->getExtra(1);
+        $reset = (float)$this->getExtra(2);
+        $val = $data[$index]["value"];
+        $out = (int)($prev != 0);
+        if (is_null($val)) {
+            if (is_null($prev)) {
+                return $prev;
+            } else {
+                return $out;
+            }
+        }
+        if ($alarm > $reset) {
+            // If the alarm value is greater than the reset value we need the
+            // following:
+            if ($val <= $reset) {
+                $out = 0;
+            }
+            if ($val >= $alarm) {
+                $out = 1;
+            }
+        } else {
+            // If the alarm value is less than the reset value we need the
+            // following:
+            if ($val >= $reset) {
+                $out = 0;
+            }
+            if ($val <= $alarm) {
+                $out = 1;
+            }
+        }
+        return $out;
+    }
+    /******************************************************************
+     ******************************************************************
+     ********  The following are input modification functions  ********
+     ******************************************************************
+     ******************************************************************/
+
+}
+?>
