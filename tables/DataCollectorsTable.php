744ed81986e2d7894b8d92cd70326e180ed09aad
Scott Price <prices@hugllc.com>
2011-03-09 20:15:49 -0600

    Fixed bug #4107.  I added GatewaysTable and DataCollectorsTable.

---
+++ tables/DataCollectorsTable.php
@@ -0,0 +1,183 @@
+<?php
+/**
+ * This is the default endpoint driver and the base for all other
+ * endpoint drivers.
+ *
+ * PHP Version 5
+ * <pre>
+ * HUGnetLib is a library of HUGnet code
+ * Copyright (C) 2007-2011 Hunt Utilities Group, LLC
+ * Copyright (C) 2009 Scott Price
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   Tables
+ * @package    HUGnetLib
+ * @subpackage Tables
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2007-2011 Hunt Utilities Group, LLC
+ * @copyright  2009 Scott Price
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    SVN: $Id$
+ * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+/** This is for the base class */
+require_once dirname(__FILE__)."/../base/HUGnetDBTable.php";
+/** This is for the configuration */
+require_once dirname(__FILE__)."/../containers/ConfigContainer.php";
+
+/**
+ * This class has functions that relate to the manipulation of elements
+ * of the devInfo array.
+ *
+ * @category   Tables
+ * @package    HUGnetLib
+ * @subpackage Tables
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2007-2011 Hunt Utilities Group, LLC
+ * @copyright  2009 Scott Price
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+class DataCollectorsTable extends HUGnetDBTable
+{
+    /** @var string This is the table we should use */
+    public $sqlTable = "datacollectors";
+    /** @var string This is the primary key of the table.  Leave blank if none  */
+    public $sqlId = "";
+    /**
+    * @var array This is the definition of the columns
+    *
+    * This should consist of the following structure:
+    * array(
+    *   "name" => array(
+    *       "Name"          => string The name of the column
+    *       "Type"          => string The type of the column
+    *       "Default"       => mixed  The default value for the column
+    *       "Null"          => bool   true if null is allowed, false otherwise
+    *       "AutoIncrement" => bool   true if the column is auto_increment
+    *       "CharSet"       => string the character set if the column is text or char
+    *       "Collate"       => string colation if the table is text or char
+    *       "Primary"       => bool   If we are a primary Key.
+    *       "Unique"        => bool   If we are a unique column.
+    *   ),
+    *   "name2" => array(
+    *   .
+    *   .
+    *   .
+    * );
+    *
+    * Not all fields have to be filled in.  Name and Type are the only required
+    * fields.  The index of the base array should be the same as the "Name" field.
+    */
+    public $sqlColumns = array(
+        "id" => array(
+            "Name" => "id",
+            "Type" => "INTEGER",
+            "Null" => false,
+        ),
+        "gatewayID" => array(
+            "Name" => "gatewayID",
+            "Type" => "int",
+            "Null" => false,
+        ),
+        "name" => array(
+            "Name" => "name",
+            "Type" => "varchar(128)",
+            "Default" => 'Unknown',
+        ),
+        "ip" => array(
+            "Name" => "ip",
+            "Type" => "varchar(128)",
+            "Default" => '',
+        ),
+        "LastContact" => array(
+            "Name" => "LastContact",
+            "Type" => "bigint",
+            "Default" => "0",
+        ),
+    );
+    /**
+    * @var array This is the definition of the indexes
+    *
+    *   array(
+    *       "Name" => array (
+    *           "Name"    => string The name of the index
+    *           "Unique"  => bool   Create a Unique index
+    *           "Columns" => array  Array of column names
+    *       ),
+    *       "name2" => array(
+    *       .
+    *       .
+    *   ),
+    */
+    public $sqlIndexes = array(
+        "id" => array(
+            "Name" => "id",
+            "Unique" => true,
+            "Columns" => array("gatewayID", "ip"),
+        ),
+    );
+
+    /** @var array This is the default values for the data */
+    protected $default = array(
+        "group" => "default",    // Server group to use
+    );
+    /**
+    * This is the constructor
+    *
+    * @param mixed $data This is an array or string to create the object from
+    */
+    function __construct($data="")
+    {
+        parent::__construct($data);
+        $this->create();
+    }
+    /**
+    * returns an object with the controller of this device in it
+    *
+    * @param int $gatewayID The gateway ID to find
+    *
+    * @return bool
+    */
+    public function &onGateway($gatewayID)
+    {
+        return $this->selectInto(
+            "`gatewayID` = ?",
+            array($gatewayID)
+        );
+    }
+
+    /******************************************************************
+     ******************************************************************
+     ********  The following are input modification functions  ********
+     ******************************************************************
+     ******************************************************************/
+    /**
+    * function to set LastHistory
+    *
+    * @param string $value The value to set
+    *
+    * @return null
+    */
+    protected function setLastContact($value)
+    {
+        $this->data["LastContact"] = self::unixDate($value);
+    }
+
+}
+?>
