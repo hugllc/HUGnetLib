a508bf35a5760099f0a6280049755f6bf1977b70
Scott Price <prices@hugllc.com>
2011-03-10 12:53:46 -0600

    These are the new files for bug #3879.  I forgot to add them in the last commit.

---
+++ base/OutputFilterBase.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * This is the default endpoint driver and the base for all other
+ * endpoint drivers.
+ *
+ * PHP Version 5
+ * <pre>
+ * HUGnetLib is a library of HUGnet code
+ * Copyright (C) 2007-2011 Hunt Utilities Group, LLC
+ * Copyright (C) 2009 Scott Price
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   Misc
+ * @package    HUGnetLib
+ * @subpackage Endpoints
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2007-2011 Hunt Utilities Group, LLC
+ * @copyright  2009 Scott Price
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    SVN: $Id$
+ * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+/** This is for the base class */
+require_once dirname(__FILE__)."/HUGnetClass.php";
+require_once dirname(__FILE__)."/../interfaces/OutputFilterInterface.php";
+
+/**
+ * This class has functions that relate to the manipulation of elements
+ * of the devInfo array.
+ *
+ * @category   Containers
+ * @package    HUGnetLib
+ * @subpackage Database
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2007-2011 Hunt Utilities Group, LLC
+ * @copyright  2009 Scott Price
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+abstract class OutputFilterBase extends HUGnetClass implements OutputFilterInterface
+{
+    /** @var The original values given to us */
+    protected $setup = array(
+    );
+    /** @var The original values given to us */
+    protected $data = null;
+
+    /**
+    * Sets everything up
+    *
+    * @param array $setup The setup for this filter
+    * @param array &$data The data to work on
+    *
+    * @return null
+    */
+    public function __construct($setup, &$data)
+    {
+        $this->data = &$data;
+        $this->setup = array_merge($this->setup, $setup);
+    }
+
+
+    /**
+    * Does the actual conversion
+    *
+    * @param mixed $field The field to execute this on
+    *
+    * @return bool True on success, false on failure
+    */
+    public function execute($field)
+    {
+        return true;
+    }
+
+}
+?>
