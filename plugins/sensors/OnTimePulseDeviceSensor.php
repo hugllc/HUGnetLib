555d50e8107f5315b3bb74f54d4dc9782daeec7d
Scott Price <prices@hugllc.com>
2011-05-27 15:21:02 -0500

    Fixed bug #4507.  I added the driver.

---
+++ plugins/sensors/OnTimePulseDeviceSensor.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Sensor driver for wind direction sensors
+ *
+ * PHP Version 5
+ *
+ * <pre>
+ * HUGnetLib is a library of HUGnet code
+ * Copyright (C) 2007-2011 Hunt Utilities Group, LLC
+ * Copyright (C) 2009 Scott Price
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   Drivers
+ * @package    HUGnetLib
+ * @subpackage Sensors
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2007-2011 Hunt Utilities Group, LLC
+ * @copyright  2009 Scott Price
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    SVN: $Id$
+ * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+ *
+ */
+/** This is for the base class */
+require_once dirname(__FILE__)."/../../base/sensors/ResistiveDeviceSensorBase.php";
+/**
+* This class deals with wind direction sensors.
+*
+* @category   Drivers
+* @package    HUGnetLib
+* @subpackage Sensors
+* @author     Scott Price <prices@hugllc.com>
+* @copyright  2007-2011 Hunt Utilities Group, LLC
+* @copyright  2009 Scott Price
+* @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+* @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+*/
+class OnTimePulseDeviceSensor extends ResistiveDeviceSensorBase
+{
+    /** @var This is to register the class */
+    public static $registerPlugin = array(
+        "Name" => "On Time Pulse Sensor",
+        "Type" => "sensor",
+        "Class" => "OnTimePulseDeviceSensor",
+        "Flags" => array("7E"),
+    );
+    /** @var object These are the valid values for units */
+    protected $idValues = array(0x7E);
+    /** @var object These are the valid values for type */
+    protected $typeValues = array("ontime");
+    /**
+    * This is the array of sensor information.
+    */
+    protected $fixed = array(
+        "longName" => "On Time Pulse Sensor",
+        "unitType" => "Percent",
+        "storageUnit" => '%',
+        "storageType" => UnitsBase::TYPE_RAW,  // This is the dataType as stored
+        "extraText" => array(),
+        // Integer is the size of the field needed to edit
+        // Array   is the values that the extra can take
+        // Null    nothing
+        "extraValues" => array(),
+        "extraDefault" => array(),
+        "maxDecimals" => 4,
+    );
+    /**
+    * Disconnects from the database
+    *
+    * @param array  $data    The servers to use
+    * @param object &$device The device we are attached to
+    */
+    public function __construct($data, &$device)
+    {
+        $this->default["id"] = 0x7E;
+        $this->default["type"] = "ontime";
+        parent::__construct($data, $device);
+    }
+
+    /**
+    * This function returns the percentage of time that the sensor is on.
+    *
+    * @param int   $A      Output of the A to D converter
+    * @param float $deltaT The time delta in seconds between this record
+    * @param array &$data  The data from the other sensors that were crunched
+    * @param mixed $prev   The previous value for this sensor
+    *
+    * @return mixed The value in whatever the units are in the sensor
+    */
+    public function getReading($A, $deltaT = 0, &$data = array(), $prev = null)
+    {
+        if ($A < 0) {
+            return null;
+        }
+        return round(
+            (float)$A/(float)($this->timeConstant * $this->Tf),
+            $this->maxDecimals
+        );
+    }
+
+}
+
+?>
