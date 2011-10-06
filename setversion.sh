df2b3ae4974e986d34712bc4d966914034fdaf7e
Scott Price <prices@hugllc.com>
2011-10-06 10:30:58 -0500

    Made the sed command a little safer for bug #4799.  I replace "-i" with "--in-place"  That way it doesn't expect a file extension.

--- setversion.sh
+++ setversion.sh
@@ -3,5 +3,5 @@
 SED=`which sed`
 
 for file in `find . -iname "*.php"`; do
-    ${SED} -i "s/@version    [ A-Za-z0-9:.$]*/@version    Release: ${1}/g" ${file}
+    ${SED} --in-place "s/@version    [ A-Za-z0-9:.$]*/@version    Release: ${1}/g" ${file}
 done
