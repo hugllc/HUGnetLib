21dc4a90d31ba6f42083d07763fef3073cf063f2
Scott Price <prices@hugllc.com>
2011-10-31 21:20:47 -0500

    Added these stubs for bug #4907.  I will flesh them out later.  I just wanted to add them so I remembered what I wanted to add.

---
+++ src/network/TransportChannel.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * This file howses the socket class
+ *
+ * PHP Version 5
+ * <pre>
+ * HUGnetLib is a library of HUGnet code
+ * Copyright (C) 2007-2011 Hunt Utilities Group, LLC
+ * Copyright (C) 2009 Scott Price
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   Libraries
+ * @package    HUGnetLib
+ * @subpackage System
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2007-2011 Hunt Utilities Group, LLC
+ * @copyright  2009 Scott Price
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+/** This is the HUGnet namespace */
+namespace HUGnet\network;
+/**
+ * This code routes packets to their correct destinations.
+ *
+ * This is the router class, essentially.  It will take packets and figure out
+ * which network interface to send them out.  This implements the Network layer
+ * of the OSI model.
+ *
+ * @category   Libraries
+ * @package    HUGnetLib
+ * @subpackage System
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2007-2011 Hunt Utilities Group, LLC
+ * @copyright  2009 Scott Price
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    Release: 0.9.7
+ * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+final class TransportChannel
+{
+    /** This is our network */
+    private $_network = array();
+    /** These are the packets we are sending */
+    private $_packets = array();
+
+    /**
+    * Sets our configuration
+    *
+    * @param array $config The configuration to use
+    */
+    private function __construct($config)
+    {
+        $this->_config = $config;
+    }
+    /**
+    * Creates the object
+    *
+    * @param array $config The configuration to use
+    *
+    * @return null
+    */
+    public function &factory($config = array())
+    {
+        return new TransportChannel((array)$config);
+    }
+
+    /**
+    * Disconnects from the database
+    *
+    */
+    public function __destruct()
+    {
+        // Shut down the network
+        unset($this->_network);
+        // Get rid of any packets
+        foreach (array_keys($this->_packets) as $key) {
+            unset($this->_packets[$key]);
+        }
+    }
+
+
+}
+?>
