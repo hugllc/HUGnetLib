f1d7c3a8b30c8b2d7839751d26ae923e3d5f9c63
Scott Price <prices@hugllc.com>
2012-03-24 20:53:03 -0500

    Work on bug #4963.  I got the base device driver class started.

---
+++ src/devices/DevDriver.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * Classes for dealing with devices
+ *
+ * PHP Version 5
+ *
+ * <pre>
+ * HUGnetLib is a library of HUGnet code
+ * Copyright (C) 2012 Hunt Utilities Group, LLC
+ * Copyright (C) 2009 Scott Price
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   Libraries
+ * @package    HUGnetLib
+ * @subpackage Devices
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2012 Hunt Utilities Group, LLC
+ * @copyright  2009 Scott Price
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+ *
+ */
+/** This is the HUGnet namespace */
+namespace HUGnet\devices;
+/** This keeps this file from being included unless HUGnetSystem.php is included */
+defined('_HUGNET') or die('HUGnetSystem not found');
+
+/**
+ * Networking for devices.
+ *
+ * This class will do all of the networking for devices.  It will poll, get configs,
+ * update software, and anything else related to talking to devices.
+ *
+ * @category   Libraries
+ * @package    HUGnetLib
+ * @subpackage Devices
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2012 Hunt Utilities Group, LLC
+ * @copyright  2009 Scott Price
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    Release: 0.9.7
+ * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+ * @since      0.9.7
+ */
+abstract class DevDriver
+{
+    /**
+    * This is where the data for the driver is stored.  This array must be
+    * put into all derivative classes, even if it is empty.
+    */
+    static protected $info = array(
+        "packetTimeout" => 6, /* This is for test value only */
+        "testParam" => "12345", /* This is for test value only */
+    );
+    /**
+    * This is where all of the defaults are stored.
+    */
+    static private $_default = array(
+        "packetTimeout" => 5,
+        "sensors" => 13,
+        "physicalSensors" => 9,
+        "virtualSensors" => 4,
+        "historyTable" => "EDEFAULTHistoryTable",
+        "averageTable" => "EDEFAULTAverageTable",
+        "loadable" => false,
+    );
+    /**
+    * This function sets up the driver object, and the database object.  The
+    * database object is taken from the driver object.
+    *
+    * @param string &$table The table object
+    *
+    * @return null
+    */
+    private function __construct()
+    {
+        /* This class shouldn't be instanciated. */
+    }
+    /**
+    * This is the destructor
+    */
+    private function __destruct()
+    {
+    }
+    /**
+    * Checks to see if a piece of data exists
+    *
+    * @param string $name The name of the property to check
+    *
+    * @return true if the property exists, false otherwise
+    */
+    public static function present($name)
+    {
+        if (isset(static::$info[$name])) {
+            return true;
+        } else if (isset(static::$_default[$name])) {
+            return true;
+        }
+        return false;
+    }
+    /**
+    * Creates the object from a string
+    *
+    * @param string $name The name of the property to get
+    *
+    * @return null
+    */
+    public static function get($name)
+    {
+        if (isset(static::$info[$name])) {
+            return static::$info[$name];
+        } else if (isset(static::$_default[$name])) {
+            return static::$_default[$name];
+        }
+        return null;
+    }
+}
+
+
+?>
