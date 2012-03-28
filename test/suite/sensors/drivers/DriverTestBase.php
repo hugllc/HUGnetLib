f6b39cde7713157f8bf2a6b31fa16eea0d5019b9
Scott Price <prices@hugllc.com>
2012-03-28 15:10:20 -0500

    Added many tests for all sensor classes for bug #5427.

--- test/suite/sensors/drivers/DriverTestBase.php
+++ test/suite/sensors/drivers/DriverTestBase.php
@@ -104,6 +104,247 @@ abstract class DriverTestBase extends \PHPUnit_Framework_TestCase
             "HUGnet\sensors\drivers\\".$this->class, get_class($this->o)
         );
     }
+
+
+
+    /**
+    * data provider for testType
+    *
+    * @return array
+    */
+    final public static function dataInternalType()
+    {
+        return array(
+            array("longName", "string"),
+            array("shortName", "string"),
+            array("extraText", "array"),
+            array("extraValues", "array"),
+            array("extraDefault", "array"),
+            array("bound", "boolean"),
+            array("virtual", "boolean"),
+            array("storageUnit", "string"),
+            array("maxDecimals", "int"),
+            array("unitType", "string"),
+        );
+    }
+    /**
+    * Check the variable type
+    *
+    * @param string $field The field to check
+    * @param string $type  The type it should be
+    *
+    * @return null
+    *
+    * @dataProvider dataInternalType
+    */
+    final public function testInternalType($field, $type)
+    {
+        $name = $this->o->get($field);
+        $this->assertInternalType($type, $name, "$field must be a $type");
+    }
+    /**
+    * data provider for testType
+    *
+    * @return array
+    */
+    final public static function dataStringSize()
+    {
+        return array(
+            array("longName", 40, 10),
+            array("shortName", 15, 1),
+            array("storageUnit", 15, 1),
+            array("unitType", 20, 1),
+        );
+    }
+    /**
+    * Check the string size
+    *
+    * @param string $field    The field to check
+    * @param int    $max      The largest it can be
+    * @param int    $min      The smallest it can be
+    *
+    * @return null
+    *
+    * @dataProvider dataStringSize
+    */
+    final public function testStringMaxSize($field, $max, $min)
+    {
+        $name = (string)$this->o->get($field);
+        $this->assertLessThanOrEqual(
+            $max, strlen($name), "$field must be less than $max chars"
+        );
+    }
+    /**
+    * Check the string size
+    *
+    * @param string $field    The field to check
+    * @param int    $max      The largest it can be
+    * @param int    $min      The smallest it can be
+    *
+    * @return null
+    *
+    * @dataProvider dataStringSize
+    */
+    final public function testStringMinSize($field, $max, $min)
+    {
+        $name = (string)$this->o->get($field);
+        $this->assertGreaterThanOrEqual(
+            $min, strlen($name), "$field must be more than $min characters"
+        );
+    }
+    /**
+    * data provider for testType
+    *
+    * @return array
+    */
+    final public static function dataIntSize()
+    {
+        return array(
+            array("maxDecimals", 10, 0),
+        );
+    }
+    /**
+    * Check the string size
+    *
+    * @param string $field    The field to check
+    * @param int    $max      The largest it can be
+    * @param int    $min      The smallest it can be
+    *
+    * @return null
+    *
+    * @dataProvider dataIntSize
+    */
+    final public function testIntMaxSize($field, $max, $min)
+    {
+        if (!is_null($max)) {
+            $name = (string)$this->o->get($field);
+            $this->assertLessThanOrEqual(
+                $max, $name, "$field must be less than $max chars"
+            );
+        }
+    }
+    /**
+    * Check the string size
+    *
+    * @param string $field    The field to check
+    * @param int    $max      The largest it can be
+    * @param int    $min      The smallest it can be
+    *
+    * @return null
+    *
+    * @dataProvider dataIntSize
+    */
+    final public function testIntMinSize($field, $max, $min)
+    {
+        if (!is_null($min)) {
+            $name = (string)$this->o->get($field);
+            $this->assertGreaterThanOrEqual(
+                $min, $name, "$field must be more than $min characters"
+            );
+        }
+    }
+    /**
+    * data provider for testType
+    *
+    * @return array
+    */
+    final public static function dataValues()
+    {
+        return array(
+            array(
+                "storageType", array(
+                    \UnitsBase::TYPE_RAW,
+                    \UnitsBase::TYPE_DIFF,
+                    \UnitsBase::TYPE_IGNORE,
+                ),
+            ),
+        );
+    }
+    /**
+    * Check the variable type
+    *
+    * @param string $field  The field to check
+    * @param array  $values The type it should be
+    *
+    * @return null
+    *
+    * @dataProvider dataValues
+    */
+    final public function testValues($field, $values)
+    {
+        $name = $this->o->get($field);
+        $this->assertTrue(
+            in_array($name, (array)$values),
+            "$field must be one of ".implode(",", (array)$values)
+        );
+    }
+
+    /**
+    * Check the extraText value size
+    *
+    * @return null
+    */
+    public function testExtraTextValueSize()
+    {
+        $size = 26;
+        $extra = (array)$this->o->get("extraText");
+        foreach($extra as $key => $value) {
+            $this->assertTrue(
+                (strlen($value) < $size),
+                "extraText[$key] must be less than $size chars"
+            );
+        }
+    }
+    /**
+    * Check the number of entries in extraText
+    *
+    * @return null
+    */
+    public function testExtraTextCount()
+    {
+        $extra   = (array)$this->o->get("extraText");
+        $default = (array)$this->o->get("extraDefault");
+        $this->assertSame(
+            count($default),
+            count($extra),
+            "extraText needs to have the same number of entries as extraDefault"
+        );
+    }
+    /**
+    * test the set routine when an extra class exists
+    *
+    * @return null
+    */
+    public function testExtraValuesElementTypes()
+    {
+        $size = 26;
+        $extra = (array)$this->o->get("extraValues");
+        foreach($extra as $key => $value) {
+            $ret = is_null($value);
+            $ret = $ret || is_array($value);
+            $ret = $ret || is_int($value);
+            $this->assertTrue(
+                $ret,
+                "extraValues[$key] must be null, an array, or an int"
+            );
+        }
+    }
+    /**
+    * Check the number of entries in extraValues
+    *
+    * @return null
+    */
+    public function testExtraValuesCount()
+    {
+        $extra   = (array)$this->o->get("extraValues");
+        $default = (array)$this->o->get("extraDefault");
+        $this->assertSame(
+            count($default),
+            count($extra),
+            "extraValues needs to have the same number of entries as extraDefault"
+        );
+    }
+
     /**
     * data provider for testDeviceID
     *
