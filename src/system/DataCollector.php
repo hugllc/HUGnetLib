21da051a17b6b7d35e3e43b672055b4a46c788d7
Scott Price <prices@hugllc.com>
2012-04-29 16:24:14 -0500

    Fixed bug #5589.  Here is the datacollector class.

---
+++ src/system/DataCollector.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Classes for dealing with devices
+ *
+ * PHP Version 5
+ *
+ * <pre>
+ * HUGnetLib is a library of HUGnet code
+ * Copyright (C) 2012 Hunt Utilities Group, LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   Libraries
+ * @package    HUGnetLib
+ * @subpackage System
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2012 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+ *
+ */
+/** This is the HUGnet namespace */
+namespace HUGnet;
+/** This keeps this file from being included unless HUGnetSystem.php is included */
+defined('_HUGNET') or die('HUGnetSystem not found');
+/** This is our base class */
+require_once dirname(__FILE__)."/../base/SystemTableBase.php";
+
+/**
+ * Base system class.
+ *
+ * This class is the new API into HUGnetLib.  It controls the config and gives out
+ * objects for everything else.  This is the only file that should be
+ *
+ * @category   Libraries
+ * @package    HUGnetLib
+ * @subpackage System
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2012 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    Release: 0.9.7
+ * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+ * @since      0.9.7
+ */
+class DataCollector extends SystemTableBase
+{
+    /**
+    * This function creates the system.
+    *
+    * @param mixed  $system (object)The system object to use
+    * @param mixed  $data   (int)The id of the item, (array) data info array
+    * @param string $table  The table to use
+    *
+    * @return null
+    */
+    public static function &factory($system, $data=null, $table="DataCollectorsTable")
+    {
+        $object = &parent::factory($system, $data, $table);
+        return $object;
+    }
+
+}
+
+
+?>
