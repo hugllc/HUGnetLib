8783fd659fd2330ae31d74f4b8638129bab9506f
Scott Price <prices@hugllc.com>
2012-06-02 21:05:41 -0500

    Created the channels object for bug #5965.

---
+++ src/php/system/Channels.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * Classes for dealing with devices
+ *
+ * PHP Version 5
+ *
+ * <pre>
+ * HUGnetLib is a library of HUGnet code
+ * Copyright (C) 2012 Hunt Utilities Group, LLC
+ * Copyright (C) 2009 Scott Price
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   Libraries
+ * @package    HUGnetLib
+ * @subpackage System
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2012 Hunt Utilities Group, LLC
+ * @copyright  2009 Scott Price
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+ *
+ */
+/** This is our namespace */
+namespace HUGnet;
+
+/** This define allows everything else to be included */
+if (!defined("_HUGNET")) {
+    define("_HUGNET", true);
+}
+/** This is the system error class.  Everybody needs it */
+require_once dirname(__FILE__).'/Error.php';
+/** This is the system utility class.  Everybody needs it also */
+require_once dirname(__FILE__).'/../util/Util.php';
+/** This is the system utility class.  Everybody needs it also */
+require_once dirname(__FILE__).'/../util/VPrint.php';
+
+
+/**
+ * Base system class.
+ *
+ * This class is the new API into HUGnetLib.  It controls the config and gives out
+ * objects for everything else.  This is the only file that should be included
+ * to get HUGnetLib functionality.  This class will load everything else it needs,
+ * so the user doesn't have to worry about it.
+ *
+ * @category   Libraries
+ * @package    HUGnetLib
+ * @subpackage System
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2012 Hunt Utilities Group, LLC
+ * @copyright  2009 Scott Price
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    Release: 0.9.7
+ * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+ * @since      0.9.7
+ */
+class Channels
+{
+    /** @var array The configuration that we are going to use */
+    private $_channels = array();
+    /** @var array The configuration that we are going to use */
+    private $_system = null;
+
+    /**
+    * This sets up the basic parts of the object for us when we create it
+    *
+    * @param object $system The system oject
+    * @param array  $config The configuration array
+    *
+    * @return null
+    */
+    private function __construct($system, $config = array())
+    {
+        System::exception(
+            get_class($this)." needs to be passed a system object",
+            "InvalidArgument",
+            !is_object($system)
+        );
+        if (is_array($config)) {
+            $this->_channels = $config;
+        }
+        $this->_sysetm = $system;
+    }
+    /**
+    * This function creates the system.
+    *
+    * @param object $system The system oject
+    * @param array  $config The configuration array
+    *
+    * @return null
+    */
+    public static function &factory($system, $config = array())
+    {
+        $obj = new Channels($system, $config);
+        return $obj;
+    }
+    /**
+    * Throws an exception
+    *
+    * @param array $record The record to convert
+    *
+    * @return null
+    */
+    public function convert($record)
+    {
+        return $record;
+    }
+    /**
+    * This function gives us access to the table class
+    *
+    * @return reference to the system object
+    */
+    public function &system()
+    {
+        return $this->_system;
+    }
+
+}
+
+
+?>
