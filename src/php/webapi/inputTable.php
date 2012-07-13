fed5ce59614e485fcfdd52e4af9c0ef83af5b513
Scott Price <prices@hugllc.com>
2012-07-13 10:11:28 -0500

    Added the api for inputTables for bug #6117.

---
+++ src/php/webapi/inputTable.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Setup Home
+ *
+ * PHP Version 5
+ * <pre>
+ * CoreUI is a user interface for the HUGnet cores.
+ * Copyright (C) 2007 Hunt Utilities Group, LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   Library
+ * @package    HUGnetLib
+ * @subpackage Webapi
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2007 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    GIT: 0.9.7
+ * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+
+
+/** This keeps this file from being included unless HUGnetSystem.php is included */
+defined('_HUGNET') or die('HUGnetSystem not found');
+require_once "HUGnetLib/tables/InputTableTable.php";
+
+$did      = hexdec($json->args()->id);
+$action = strtolower($json->args()->action);
+$arch = substr((string)$_REQUEST["arch"], 0, 6);
+$input = new InputTableTable();
+
+if ($action === "get") {
+    $input->getRow($did);
+    $ret = $input->toArray();
+} else if ($action == "ids") {
+    $where = "";
+    $whereData = array();
+    if (!empty($arch)) {
+        $where      .= "arch = ?";
+        $whereData[] = $arch;
+    }
+    $ret = $input->selectIDs($where, $whereData);
+} else if ($action === "getall") {
+    $where = "";
+    $whereData = array();
+    if (!empty($arch)) {
+        $where      .= "arch = ?";
+        $whereData[] = $arch;
+    }
+    $ids = $input->selectIDs($where, $whereData);
+    $ret = array();
+    foreach ((array)$ids as $value) {
+        $input->getRow((int)$value);
+        $ret[] = $input->toArray();
+    }
+}
+
+
+print json_encode($ret);
+
+?>
