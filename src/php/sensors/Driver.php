cd010509fa3be995896817a0c48abf1c7522e099
Scott Price <prices@hugllc.com>
2012-11-05 10:45:52 -0600

    Added the Baro4 pressure sensor for bug #6069.

--- src/php/sensors/Driver.php
+++ src/php/sensors/Driver.php
@@ -114,6 +114,7 @@ abstract class Driver
         "04:DEFAULT"                 => "ADuCVishayRTD",
         "11:DEFAULT"                 => "ADuCPower",
         "40:ControllerVoltage"       => "ControllerVoltage",
+        "40:BARO4"                   => "AVRBAROA4V",
         "41:DEFAULT"                 => "ADuCVoltage",
         "41:ADuCPressure"            => "ADuCPressure",
         "42:DEFAULT"                 => "ADuCThermocouple",
