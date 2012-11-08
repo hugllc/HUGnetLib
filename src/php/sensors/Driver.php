ecea57c9bf58b1492c4483ce0f58d1f5f15cbb41
Scott Price <prices@hugllc.com>
2012-11-08 11:54:03 -0600

    Fixed bug #6837.  I added CelaniPowerCalVirtual driver to do these calculations.

--- src/php/sensors/Driver.php
+++ src/php/sensors/Driver.php
@@ -152,6 +152,7 @@ abstract class Driver
         "FE:DEFAULT"                 => "EmptyVirtual",
         "FE:AlarmVirtual"            => "AlarmVirtual",
         "FE:BinaryVirtual"           => "BinaryVirtual",
+        "FE:CelaniPower"             => "CelaniPowerCalVirtual",
         "FE:CloneVirtual"            => "CloneVirtual",
         "FE:ComputationVirtual"      => "ComputationVirtual",
         "FE:DewPointVirtual"         => "DewPointVirtual",
