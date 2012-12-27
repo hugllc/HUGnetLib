135db5ffb431921ac8fab5ad5ed2655a5111f7ba
Scott Price <prices@hugllc.com>
2012-12-27 08:51:02 -0600

    Work on bug #7349.  I added the files.  Now I have to test them.

--- sql/mysql/0.10.0/renamesensors.sql
+++ sql/mysql/0.10.0/renamesensors.sql
@@ -1,3 +1,4 @@
-RENAME TABLE `HUGnet`.`sensors` TO `HUGnet`.`deviceInputs` ;
+RENAME TABLE `sensors` TO `deviceInputs` ;
 ALTER TABLE `deviceInputs` CHANGE `sensor` `input` INT( 11 ) NOT NULL;
 ALTER TABLE `deviceInputs` DROP `dataType`, DROP `units`, DROP `decimals`;
+ALTER TABLE `deviceInputs` ADD `calibration` TEXT NOT NULL AFTER `driver`;
