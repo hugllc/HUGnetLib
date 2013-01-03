c2cf5ae862f381fdffd34a6299d56f9f29ba5097
Scott Price <prices@hugllc.com>
2013-01-03 09:40:55 -0600

    Revert "This fixes #7357.  It now waits 10 seconds and prints out a message saying that the connection was lost."
    This reverts commit 98eec493731d273ac85e7c0166137f10d48c795a.
    
    This was causing everything in the processes to stop.  That was not acceptable.

--- src/php/network/physical/Socket.php
+++ src/php/network/physical/Socket.php
@@ -185,12 +185,6 @@ final class Socket
             if (socket_last_error($this->_socket) > 0) {
                 socket_clear_error($this->_socket);
                 $this->_disconnect();
-                \HUGnet\VPrint::out(
-                    "Socket ".$this->_config["location"]." disappeared.  "
-                    ."Waiting 10s",
-                    1
-                );
-                sleep(10);
                 $this->_connect();
             }
             \HUGnet\VPrint::out(
