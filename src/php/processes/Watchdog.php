24182db6cafa2c7ec006e6d07c1b6efbdcb903c3
Scott Price <prices@hugllc.com>
2013-02-02 09:19:53 -0600

    Some work on bug #7393.  I got the basic structure in place.  Now I just have to add the email output, and the plugins to check for things.

---
+++ src/php/processes/Watchdog.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * This file howses the socket class
+ *
+ * PHP Version 5
+ * <pre>
+ * HUGnetLib is a library of HUGnet code
+ * Copyright (C) 2013 Hunt Utilities Group, LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   Libraries
+ * @package    HUGnetLib
+ * @subpackage UI
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2013 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link       http://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+/** This is the HUGnet namespace */
+namespace HUGnet\processes;
+/** This is our base class */
+require_once dirname(__FILE__)."/../ui/Daemon.php";
+/** This is our base class */
+require_once dirname(__FILE__)."/watchdog/Periodic.php";
+
+/**
+ * This code routes packets to their correct destinations.
+ *
+ * This is the router class, essentially.  It will take packets and figure out
+ * which network interface to send them out.  This implements the Network layer
+ * of the OSI model.
+ *
+ * @category   Libraries
+ * @package    HUGnetLib
+ * @subpackage UI
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2013 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    Release: 0.10.2
+ * @link       http://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+class Watchdog extends \HUGnet\ui\Daemon
+{
+    /** This is the amount of time we wait */
+    const WAIT_TIME = 30;
+
+    /** This is the start time of the current run */
+    private $_mainStart;
+    /** How long we should wait */
+    private $_wait;
+    /** This is my ID */
+    private $_myID;
+    /** This is the start time of the current run */
+    private $_plugins = array();
+    /**
+    * Sets our configuration
+    *
+    * @param mixed &$config The configuration to use
+    */
+    protected function __construct(&$config)
+    {
+        parent::__construct($config);
+        /* Get our Device */
+        $this->_plugins = \HUGnet\processes\watchdog\Periodic::plugins($this);
+    }
+    /**
+    * Creates the object
+    *
+    * @param array &$config The configuration to use
+    *
+    * @return null
+    */
+    static public function &factory(&$config = array())
+    {
+        $obj = new Watchdog($config);
+        return $obj;
+    }
+    /**
+    * This is the destructor
+    */
+    public function __destruct()
+    {
+        foreach (array_keys((array)$this->_plugins) as $key) {
+            unset($this->_plugins[$key]);
+        }
+    }
+    /**
+    * This is our main routine
+    *
+    * @return null
+    */
+    public function main()
+    {
+        $this->_mainStart = time();
+        foreach ($this->_plugins as $obj) {
+            $obj->execute();
+        }
+        $this->_wait();
+    }
+    /**
+    * This is our wait
+    *
+    * @return null
+    */
+    private function _wait()
+    {
+        parent::main(); // This should be called at least once per iteration
+        $this->_wait = self::WAIT_TIME - (time() - $this->_mainStart);
+        if (($this->_wait > 0) && $this->loop()) {
+            $this->out("Waiting ".$this->_wait." seconds at ".date("Y-m-d H:i:s"));
+            for (; ($this->_wait > 0) && $this->loop(); $this->_wait--) {
+                parent::main();
+                sleep(1);
+            }
+        }
+    }
+    /**
+    * Deals with incoming packets
+    *
+    * @param object $pkt The packet to send out
+    *
+    * @return null
+    */
+    public function packet($pkt)
+    {
+        if ($pkt->type() === "RECONFIG") {
+
+        }
+    }
+    /**
+    * Creates the object
+    *
+    * @param array $config The configuration to use
+    *
+    * @return null
+    */
+    public function &device($config = array())
+    {
+        return false;
+    }
+}
+?>
---
+++ src/php/processes/watchdog/Periodic.php
@@ -0,0 +1,219 @@
+<?php
+/**
+ * Classes for dealing with devices
+ *
+ * PHP Version 5
+ *
+ * <pre>
+ * HUGnetLib is a library of HUGnet code
+ * Copyright (C) 2013 Hunt Utilities Group, LLC
+ * Copyright (C) 2009 Scott Price
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   Libraries
+ * @package    HUGnetLib
+ * @subpackage Devices
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2013 Hunt Utilities Group, LLC
+ * @copyright  2009 Scott Price
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link       http://dev.hugllc.com/index.php/Project:HUGnetLib
+ *
+ */
+/** This is the HUGnet namespace */
+namespace HUGnet\processes\watchdog;
+/** This keeps this file from being included unless HUGnetSystem.php is included */
+defined('_HUGNET') or die('HUGnetSystem not found');
+
+/**
+ * Base driver class for devices.
+ *
+ * This class deals with loading the drivers and figuring out what driver needs
+ * to be loaded.
+ *
+ * @category   Libraries
+ * @package    HUGnetLib
+ * @subpackage Devices
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2013 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    Release: 0.10.2
+ * @link       http://dev.hugllc.com/index.php/Project:HUGnetLib
+ * @since      0.9.7
+ */
+abstract class Periodic
+{
+    /**
+    * the period in seconds
+    */
+    protected $period = 0;
+    /**
+    * the last time we ran
+    */
+    protected $last = 0;
+    /**
+    * the system object
+    */
+    private $_ui = null;
+    /**
+    * This is where all of the driver information is stored.
+    *
+    * Drivers must be registered here, otherwise they will never get loaded.  The
+    * index in the root array is the driver name.  It should be exactly the same
+    * as the driver class name.
+    */
+    private static $_drivers = array(
+        "CheckMysql",
+    );
+    /**
+    * This function sets up the driver object, and the database object.  The
+    * database object is taken from the driver object.
+    *
+    * @param object &$gui The user interface to use
+    *
+    * @return null
+    */
+    protected function __construct(&$gui)
+    {
+        $this->_ui = &$gui;
+    }
+    /**
+    * This is the destructor
+    */
+    public function __destruct()
+    {
+    }
+    /**
+    * This function creates the system.
+    *
+    * @param object &$gui The user interface to use
+    *
+    * @return null
+    */
+    protected static function &intFactory(&$gui)
+    {
+        $class = get_called_class();
+        $object = new $class($gui);
+        return $object;
+    }
+    /**
+    * This function creates the system.
+    *
+    * @return null
+    */
+    abstract public function &execute();
+    /**
+    * This function creates the system.
+    *
+    * @param object &$gui the user interface object
+    *
+    * @return null
+    */
+    public static function &plugins(&$gui)
+    {
+        $plugins = array();
+        foreach (self::$_drivers as $driver) {
+            $class = \HUGnet\Util::findClass(
+                $driver,
+                "processes/watchdog/periodic",
+                true,
+                "\\HUGnet\\processes\\watchdog\\periodic"
+            );
+            if (class_exists($class)) {
+                $plugins[$class] = $class::factory($gui);
+            }
+
+        }
+        return (array)$plugins;
+    }
+    /**
+    * This says if we are ready to run
+    *
+    * @return bool
+    */
+    protected function ready()
+    {
+        return ($this->period < (time() - $this->last)) && ($this->period != 0);
+    }
+    /**
+    * This says if we are ready to run
+    *
+    * @return bool
+    * @SuppressWarnings(PHPMD.ShortMethodName)
+    */
+    protected function &ui()
+    {
+        return $this->_ui;
+    }
+    /**
+    * This says if we are ready to run
+    *
+    * @return bool
+    */
+    public function &system()
+    {
+        return $this->_ui->system();
+    }
+    /**
+    * This says if we are ready to run
+    *
+    * @return bool
+    */
+    public function &device()
+    {
+        return $this->system()->device(
+            $this->system()->network()->device()->getID()
+        );
+    }
+    /**
+    * This says if we are ready to run
+    *
+    * @return bool
+    */
+    protected function success()
+    {
+        $this->last = time();
+        $this->system()->out(
+            "Success.  Next run ".date("Y-m-d H:i:s", $this->last + $this->period)
+        );
+    }
+    /**
+    * This says if we are ready to run
+    *
+    * @return bool
+    */
+    protected function failure()
+    {
+        $this->last = (time() - $this->period + 60);
+        $this->system()->out(
+            "Failure. Will try again in 1 minute"
+        );
+    }
+    /**
+    * This says if we are ready to run
+    *
+    * @return bool
+    */
+    protected function hasMaster()
+    {
+        $master = $this->system()->get("master");
+        return is_array($master) && (count($master) > 0);
+    }
+}
+
+
+?>
