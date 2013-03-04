4b62d5b5b15823e4c64d4951df9856d2a7b6bc75
Scott Price <prices@hugllc.com>
2013-03-03 19:47:17 -0600

    Work on bug #7574.  The input tables are done.

--- src/php/devices/inputTable/Driver.php
+++ src/php/devices/inputTable/Driver.php
@@ -37,18 +37,20 @@
 namespace HUGnet\devices\inputTable;
 /** This keeps this file from being included unless HUGnetSystem.php is included */
 defined('_HUGNET') or die('HUGnetSystem not found');
+/** This is our base class */
+require_once dirname(__FILE__)."/../../base/LoadableDriver.php";
 /** This is our interface */
 require_once dirname(__FILE__)."/DriverInterface.php";
 /** This is our units class */
 require_once dirname(__FILE__)."/../datachan/Driver.php";
 /** This is our units class */
-require_once dirname(__FILE__)."/DriverADuC.php";
+//require_once dirname(__FILE__)."/DriverADuC.php";
 /** This is our units class */
-require_once dirname(__FILE__)."/DriverAVR.php";
+//require_once dirname(__FILE__)."/DriverAVR.php";
 /** This is our units class */
-require_once dirname(__FILE__)."/DriverVirtual.php";
+//require_once dirname(__FILE__)."/DriverVirtual.php";
 /** This is our units class */
-require_once dirname(__FILE__)."/DriverLinux.php";
+//require_once dirname(__FILE__)."/DriverLinux.php";
 
 /**
  * Base driver class for devices.
@@ -69,7 +71,7 @@ require_once dirname(__FILE__)."/DriverLinux.php";
  * @SuppressWarnings(PHPMD.ShortVariable)
  * @SuppressWarnings(PHPMD.NumberOfChildren)
  */
-abstract class Driver
+abstract class Driver extends \HUGnet\base\LoadableDriver
 {
     /**
     * This is where we store our float size information
@@ -95,7 +97,7 @@ abstract class Driver
     /**
     * This is where all of the defaults are stored.
     */
-    private $_default = array(
+    protected $default = array(
         "longName" => "",
         "shortName" => "",
         "unitType" => "",
@@ -277,6 +279,14 @@ abstract class Driver
     */
     public static function &factory($driver, &$sensor, $table = null)
     {
+        /** This is the ADuC Class */
+        include_once dirname(__FILE__)."/DriverADuC.php";
+        /** This is the AVR Class */
+        include_once dirname(__FILE__)."/DriverAVR.php";
+        /** This is the virtual driver class */
+        include_once dirname(__FILE__)."/DriverVirtual.php";
+        /** This is our Linux driver class */
+        include_once dirname(__FILE__)."/DriverLinux.php";
         $obj = null;
         Driver::driverFactory($obj, $driver, $sensor, $table);
         DriverADuC::driverFactory($obj, $driver, $sensor, $table);
@@ -316,52 +326,6 @@ abstract class Driver
             return true;
         }
         return false;
-    }
-    /**
-    * Checks to see if a piece of data exists
-    *
-    * @param string $name The name of the property to check
-    *
-    * @return true if the property exists, false otherwise
-    */
-    public function present($name)
-    {
-        return !is_null($this->get($name, $this->input()));
-    }
-    /**
-    * Gets an item
-    *
-    * @param string $name The name of the property to get
-    *
-    * @return null
-    */
-    public function get($name)
-    {
-        $ret = null;
-        if (isset($this->params[$name])) {
-            $ret = $this->params[$name];
-        } else if (isset($this->_default[$name])) {
-            $ret = $this->_default[$name];
-        }
-        if (is_string($ret) && (strtolower(substr($ret, 0, 8)) === "getextra")) {
-            $key = (int)substr($ret, 8);
-            $ret = $this->getExtra($key);
-        }
-        return $ret;
-    }
-    /**
-    * Returns all of the parameters and defaults in an array
-    *
-    * @return array of data from the sensor
-    */
-    public function toArray()
-    {
-        $return = array();
-        $keys = array_merge(array_keys($this->_default), array_keys($this->params));
-        foreach ($keys as $key) {
-            $return[$key] = $this->get($key);
-        }
-        return $return;
     }
     /**
     * Returns the driver that should be used for a particular device
@@ -474,30 +438,6 @@ abstract class Driver
             $extra = $this->get("extraDefault");
         }
         return $extra[$index];
-    }
-
-    /**
-    * Decodes the driver portion of the setup string
-    *
-    * @param string $string The string to decode
-    *
-    * @return array
-    * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-    */
-    public function decode($string)
-    {
-        /* Do nothing by default */
-    }
-    /**
-    * Encodes this driver as a setup string
-    *
-    * @return array
-    * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-    */
-    public function encode()
-    {
-        $string  = "";
-        return $string;
     }
 
     /**
@@ -786,118 +726,6 @@ abstract class Driver
         }
         $val = ($ppm / 60) * $deltaT;
         return (int)$val;
-    }
-    /**
-    * This builds the string for the levelholder.
-    *
-    * @param int $val   The value to use
-    * @param int $bytes The number of bytes to set
-    *
-    * @return string The string
-    */
-    protected function encodeInt($val, $bytes = 4)
-    {
-        $val = (int)$val;
-        for ($i = 0; $i < $bytes; $i++) {
-            $str .= sprintf(
-                "%02X",
-                ($val >> ($i * 8)) & 0xFF
-            );
-        }
-        return $str;
-
-    }
-    /**
-    * This builds the string for the levelholder.
-    *
-    * @param string $val    The value to use
-    * @param int    $bytes  The number of bytes to set
-    * @param bool   $signed If the number is signed or not
-    *
-    * @return string The string
-    */
-    protected function decodeInt($val, $bytes = 4, $signed = false)
-    {
-        $int = 0;
-        for ($i = 0; $i < $bytes; $i++) {
-            $int += hexdec(substr($val, ($i * 2), 2))<<($i * 8);
-        }
-        $bits = $bytes * 8;
-        $int = (int)($int & (pow(2, $bits) - 1));
-        if ($signed) {
-            /* Calculate the top bit */
-            $topBit = pow(2, ($bits - 1));
-            /* Check to see if the top bit is set */
-            if (($int & $topBit) == $topBit) {
-                /* This is a negative number */
-                $int = -(pow(2, $bits) - $int);
-            }
-
-        }
-        return $int;
-
-    }
-    /**
-    * This builds the string for the levelholder.
-    *
-    * @param float $val   The value to use
-    * @param int   $bytes The number of bytes to use
-    *
-    * @return string The string
-    */
-    protected function encodeFloat($val, $bytes = 4)
-    {
-        if (!isset($this->_floats[(int)$bytes])) {
-            return $this->encodeInt(null, $bytes);
-        }
-        $bits  = $this->_floats[(int)$bytes]["bits"];
-        $esize = $this->_floats[(int)$bytes]["esize"];
-        $ebias = $this->_floats[(int)$bytes]["ebias"];
-        $fsize = $this->_floats[(int)$bytes]["fsize"];
-
-        $sign = ($val < 0) ? 1 : 0;
-        $val = abs($val);
-        $exp  = 0;
-        // This sections makes it a number between 1 and 2
-        while ($val >= 2) {
-            $val /= 2;
-            $exp++;
-        }
-        while ($val < 1) {
-            $val *= 2;
-            $exp--;
-        }
-        $exp += $ebias;
-        $int  = round((($val - 1) * pow(2, $fsize)));
-        $int  = $int | ($exp << $fsize) | ($sign << ($bits - 1));
-        return $this->encodeInt($int);
-
-    }
-    /**
-    * This builds the string for the levelholder.
-    *
-    * @param string $val   The value to use
-    * @param int    $bytes The number of bytes to use
-    *
-    * @return string The string
-    */
-    protected function decodeFloat($val, $bytes = 4)
-    {
-        if (!isset($this->_floats[(int)$bytes])) {
-            return null;
-        }
-        // First, we need to get the int
-        $int   = $this->decodeInt($val, $bytes, false);
-        $bits  = $this->_floats[(int)$bytes]["bits"];
-        $esize = $this->_floats[(int)$bytes]["esize"];
-        $ebias = $this->_floats[(int)$bytes]["ebias"];
-        $fsize = $this->_floats[(int)$bytes]["fsize"];
-        $sign  = ($int & pow(2, $bits - 1)) ? -1 : 1;
-        $fract = 1 + ((float)($int & (pow(2, $fsize) - 1)) / (float)pow(2, $fsize));
-        $exp   = (($int >> $fsize) & 0xFF) - $ebias;
-        $float = $fract * pow(2, $exp) * $sign;
-        return $float;
-
     }
 }
 
