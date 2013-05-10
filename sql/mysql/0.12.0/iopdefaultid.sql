c328c63e9f22332ca5464e4ea9f3fe43684b1e3e
Scott Price <prices@hugllc.com>
2013-05-10 13:39:31 -0500

    Added in an sql query that should fix #7797.  It should change on installation.

---
+++ sql/mysql/0.12.0/iopdefaultid.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `deviceInputs` CHANGE `id` `id` INT( 11 ) NOT NULL DEFAULT '255';
+ALTER TABLE `deviceOutputs` CHANGE `id` `id` INT( 11 ) NOT NULL DEFAULT '255';
+ALTER TABLE `deviceProcesses` CHANGE `id` `id` INT( 11 ) NOT NULL DEFAULT '255';
