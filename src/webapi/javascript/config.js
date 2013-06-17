2cf65a9c2763f91e0232132b764ba00bf52e5956
Scott Price <prices@hugllc.com>
2013-06-17 09:47:21 -0500

    A fix for bug #7874.  I was overwriting the inputtable setup.

--- src/webapi/javascript/config.js
+++ src/webapi/javascript/config.js
@@ -103,9 +103,9 @@ HUGnet.Config = Backbone.View.extend({
         this.tabs.find( ".ui-tabs-nav" ).append('<li><a href="#'+this.id+'-inputTable">Input Tables</a></li>');
         this.tabs.append('<div id="'+this.id+'-inputTable"></div>');
         $('#'+this.id+'-inputTable').html(this.inputTables.render().el);
-        this.tabs.find( ".ui-tabs-nav" ).append('<li><a href="#'+this.id+'-inputTable">Runtime Config</a></li>');
-        this.tabs.append('<div id="'+this.id+'-inputTable"></div>');
-        $('#'+this.id+'-inputTable').html(this.runtime.render().el);
+        this.tabs.find( ".ui-tabs-nav" ).append('<li><a href="#'+this.id+'-Runtime">Runtime Config</a></li>');
+        this.tabs.append('<div id="'+this.id+'-Runtime"></div>');
+        $('#'+this.id+'-Runtime').html(this.runtime.render().el);
         /*
         this.tabs.find( ".ui-tabs-nav" ).append('<li><a href="#'+this.id+'-outputTable">Output Tables</a></li>');
         this.tabs.append('<div id="'+this.id+'-outputTable"></div>');
