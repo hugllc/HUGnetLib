fe127afef95833e7a7ddeae6129b12adcfc67842
Scott Price <prices@hugllc.com>
2013-06-21 16:43:08 -0500

    This is work on bug #7906.  I got the framework in place, now I just need to add in the details.

---
+++ src/php/system/Fixture.php
@@ -0,0 +1,243 @@
+<?php
+/**
+ * Classes for dealing with devices
+ *
+ * PHP Version 5
+ *
+ * <pre>
+ * HUGnetLib is a library of HUGnet code
+ * Copyright (C) 2013 Hunt Utilities Group, LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   Libraries
+ * @package    HUGnetLib
+ * @subpackage System
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2013 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link       http://dev.hugllc.com/index.php/Project:HUGnetLib
+ *
+ */
+/** This is the HUGnet namespace */
+namespace HUGnet;
+/** This keeps this file from being included unless HUGnetSystem.php is included */
+defined('_HUGNET') or die('HUGnetSystem not found');
+/** This is our base class */
+require_once dirname(__FILE__)."/Device.php";
+/** This is our base class */
+require_once dirname(__FILE__)."/../interfaces/WebAPI.php";
+/** This is our system interface */
+require_once dirname(__FILE__)."/../interfaces/SystemInterface.php";
+/* THis is our outputs */
+require_once dirname(__FILE__)."/../devices/Output.php";
+
+/**
+ * Base system class.
+ *
+ * This class is the new API into HUGnetLib.  It controls the config and gives out
+ * objects for everything else.  This is the only file that should be
+ *
+ * @category   Libraries
+ * @package    HUGnetLib
+ * @subpackage System
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2013 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    Release: 0.13.0
+ * @link       http://dev.hugllc.com/index.php/Project:HUGnetLib
+ * @since      0.13.0
+ */
+class Fixture extends \HUGnet\Device
+    implements \HUGnet\interfaces\SystemInterface
+{
+    /**
+    * This is the destructor
+    */
+    public function __destruct()
+    {
+        parent::__destruct();
+    }
+    /**
+    * This function creates the system.
+    *
+    * @param mixed  &$system (object)The system object to use
+    * @param mixed  $data    (int)The id of the item, (array) data info array
+    * @param string $table   The table to use
+    *
+    * @return null
+    */
+    public static function &factory(&$system, $data=null, $table="Fixtures")
+    {
+        $object = parent::factory($system, $data, $table);
+        return $object;
+    }
+    /**
+    * Gets a value
+    *
+    * @param string $field the field to get
+    *
+    * @return null
+    */
+    public function get($field)
+    {
+    }
+    /**
+    * Returns the table as an array
+    *
+    * @param bool $default Whether or not to include the default values
+    *
+    * @return array
+    */
+    public function toArray($default = false)
+    {
+    }
+    /**
+    * This takes the class and makes it into a setup string
+    *
+    * @param bool $showFixed Show the fixed portion of the data
+    *
+    * @return string The encoded string
+    */
+    public function export($showFixed = true)
+    {
+    }
+    /**
+    * This builds the class from a setup string
+    *
+    * @param string $string The setup string to decode
+    *
+    * @return bool True on success, false on failure
+    */
+    public function import($string)
+    {
+    }
+    /**
+    * This creates the sensor drivers
+    *
+    * @param mixed $chans Channel information
+    *
+    * @return null
+    */
+    public function &dataChannels($chans = null)
+    {
+        include_once dirname(__FILE__)."/../devices/DataChannels.php";
+        return \HUGnet\devices\DataChannels::factory($this->system(), $this, $chans);
+    }
+    /**
+    * This creates the sensor drivers
+    *
+    * @param mixed $chans Channel information
+    *
+    * @return null
+    */
+    public function &controlChannels($chans = null)
+    {
+        include_once dirname(__FILE__)."/../devices/ControlChannels.php";
+        return \HUGnet\devices\ControlChannels::factory(
+            $this->system(), $this, $chans
+        );
+    }
+
+    /**
+    * returns a history object for this device
+    *
+    * @param array $data    The data to build the history record with.
+    * @param bool  $history History if true, average if false
+    *
+    * @return string
+    */
+    public function &historyFactory($data = array(), $history = true)
+    {
+        $class = $this->driver()->historyTable($history);
+        $obj = $this->system()->table($class, $data);
+        $obj->device = &$this;
+        return $obj;
+    }
+    /**
+    * This creates the sensor drivers
+    *
+    * @param int $sid The sensor id to get.  They are labaled 0 to sensors
+    *
+    * @return null
+    */
+    public function &input($sid)
+    {
+        unset($info["data"]["dev"]);
+        unset($info["data"]["input"]);
+        include_once dirname(__FILE__)."/../devices/Input.php";
+        $system = $this->system();
+        $ret = \HUGnet\devices\Input::factory(
+            $system,
+            array("dev" => $this->id(), "input" => $sid),
+            null,
+            $this,
+            (array)$info["table"]
+        );
+        return $ret;
+
+        return $this->driver()->input($sid);
+    }
+    /**
+    * This creates the sensor drivers
+    *
+    * @param int $sid The sensor id to get.  They are labaled 0 to sensors
+    *
+    * @return null
+    */
+    public function &output($sid)
+    {
+        unset($info["data"]["dev"]);
+        unset($info["data"]["output"]);
+        $system = $this->system();
+        $ret = \HUGnet\devices\Output::factory(
+            $system,
+            array("dev" => $this->id(), "output" => $sid),
+            null,
+            $this,
+            (array)$info["table"]
+        );
+        return $ret;
+
+        return $this->driver()->output($sid);
+    }
+    /**
+    * This creates the sensor drivers
+    *
+    * @param int $sid The sensor id to get.  They are labaled 0 to sensors
+    *
+    * @return null
+    */
+    public function &process($sid)
+    {
+        include_once dirname(__FILE__)."/../devices/Process.php";
+        unset($info["data"]["dev"]);
+        unset($info["data"]["process"]);
+        $system = $this->system();
+        $ret = \HUGnet\devices\Process::factory(
+            $system,
+            array("dev" => $this->id(), "process" => $sid),
+            null,
+            $this,
+            (array)$info["table"]
+        );
+        return $ret;
+    }
+
+}
+
+
+?>
