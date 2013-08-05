23fac2c7ab698576b7be95af59b3770909469964
Scott Price <prices@hugllc.com>
2013-08-05 10:31:08 -0500

    This is a fix for bug #7964.  I had an SQL query wrong.

--- sql/mysql/0.13.0/tableentry.sql
+++ sql/mysql/0.13.0/tableentry.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `deviceInputs` ADD `tableEntry` TEXT NOT NULL AFTER `calibration`;
 ALTER TABLE `deviceOutputs` ADD `tableEntry` TEXT NOT NULL AFTER `driver`;
 ALTER TABLE `deviceProcesses` ADD `tableEntry` TEXT NOT NULL AFTER `driver`;
-UPDATE inputTable` SET `arch`= "0039-37" WHERE `arch` = "ADuC";
+UPDATE `inputTable` SET `arch` = "0039-37" WHERE `arch` = "ADuC";
 ALTER TABLE `devices` ADD `Publish` TINYINT NOT NULL DEFAULT '1' AFTER `Active`;
