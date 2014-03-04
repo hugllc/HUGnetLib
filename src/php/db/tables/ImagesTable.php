5f709a99ed71a88dfe64b98ba35aeb2ac7d3fa2b
Scott Price <prices@hugllc.com>
2014-03-03 21:09:10 -0600

    Work on bug #8138.  This is the database table to store images in.

---
+++ src/php/db/tables/ImagesTable.php
@@ -0,0 +1,207 @@
+<?php
+/**
+ * This is the default endpoint driver and the base for all other
+ * endpoint drivers.
+ *
+ * PHP Version 5
+ * <pre>
+ * HUGnetLib is a library of HUGnet code
+ * Copyright (C) 2013 Hunt Utilities Group, LLC
+ * Copyright (C) 2009 Scott Price
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   Libraries
+ * @package    HUGnetLib
+ * @subpackage Tables
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2013 Hunt Utilities Group, LLC
+ * @copyright  2009 Scott Price
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link       http://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+namespace HUGnet\db\tables;
+/** This keeps this file from being included unless HUGnetSystem.php is included */
+defined('_HUGNET') or die('HUGnetSystem not found');
+/** This is our system interface */
+require_once dirname(__FILE__)."/../../interfaces/DBTable.php";
+
+/**
+ * This class has functions that relate to the manipulation of elements
+ * of the devInfo array.
+ *
+ * @category   Libraries
+ * @package    HUGnetLib
+ * @subpackage Tables
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2013 Hunt Utilities Group, LLC
+ * @copyright  2009 Scott Price
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    Release: 0.10.2
+ * @link       http://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+class ImagesTable extends \HUGnet\db\Table implements \HUGnet\interfaces\DBTable
+{
+    /** @var string This is the table we should use */
+    public $sqlTable = "images";
+    /** @var string This is the primary key of the table.  Leave blank if none  */
+    public $sqlId = "id";
+    /**
+    * @var array This is the definition of the columns
+    *
+    * This should consist of the following structure:
+    * array(
+    *   "name" => array(
+    *       "Name"          => string The name of the column
+    *       "Type"          => string The type of the column
+    *       "Default"       => mixed  The default value for the column
+    *       "Null"          => bool   true if null is allowed, false otherwise
+    *       "AutoIncrement" => bool   true if the column is auto_increment
+    *       "CharSet"       => string the character set if the column is text or char
+    *       "Collate"       => string colation if the table is text or char
+    *       "Primary"       => bool   If we are a primary Key.
+    *       "Unique"        => bool   If we are a unique column.
+    *   ),
+    *   "name2" => array(
+    *   .
+    *   .
+    *   .
+    * );
+    *
+    * Not all fields have to be filled in.  Name and Type are the only required
+    * fields.  The index of the base array should be the same as the "Name" field.
+    */
+    public $sqlColumns = array(
+        "id" => array(
+            "Name" => "id",
+            "Type" => "INTEGER",
+            "AutoIncrement" => true,
+            "Primary" => true,
+        ),
+        "name" => array(
+            "Name" => "name",
+            "Type" => "varchar(64)",
+            "Default" => "",
+        ),
+        "image" => array(
+            "Name" => "image",
+            "Type" => "longblob",
+            "Default" => '',
+        ),
+        "desc" => array(
+            "Name" => "desc",
+            "Type" => "text",
+            "Default" => '',
+        ),
+        "points" => array(
+            "Name" => "points",
+            "Type" => "longtext",
+            "Default" => "",
+        ),
+    );
+    /**
+    * @var array This is the definition of the indexes
+    *
+    *   array(
+    *       "Name" => array (
+    *           "Name"    => string The name of the index
+    *           "Unique"  => bool   Create a Unique index
+    *           "Columns" => array  Array of column names
+    *       ),
+    *       "name2" => array(
+    *       .
+    *       .
+    *   ),
+    */
+    public $sqlIndexes = array(
+        "name" => array(
+            "Name" => "name",
+            "Columns" => array("name"),
+        ),
+        "id" => array(
+            "Name" => "id",
+            "Columns" => array("id"),
+        ),
+    );
+
+    /** @var array This is the default values for the data */
+    protected $default = array(
+        "group" => "default",    // Server group to use
+    );
+    /**
+    * This is the constructor
+    *
+    * @param object &$system  The system object to use
+    * @param mixed  $data     This is an array or string to create the object from
+    * @param object &$connect The connection manager
+    */
+    protected function __construct(&$system, $data="", &$connect = null)
+    {
+        parent::__construct($system, $data, $connect);
+        $this->create();
+    }
+
+    /**
+    * Checks to see if our deviceID exists in the database
+    *
+    * @return bool True if it exists, false otherwise
+    */
+    public function exists()
+    {
+
+        $ret = (bool) $this->dbDriver()->countWhere(
+            "id = ?", array($this->get("id")), "id"
+        );
+        $this->dbDriver()->reset();
+        return $ret;
+    }
+    /**
+    * Sets all of the endpoint attributes from an array
+    *
+    * @param bool $default Return items set to their default?
+    *
+    * @return null
+    */
+    public function toArray($default = true)
+    {
+        $data   = parent::toArray($default);
+        $data["points"] = json_decode($data["points"], true);
+        return array_merge((array)$params, (array)$data);
+    }
+
+    /******************************************************************
+     ******************************************************************
+     ********  The following are input modification functions  ********
+     ******************************************************************
+     ******************************************************************/
+    /**
+    * function to set LastHistory
+    *
+    * @param string $value The value to set
+    *
+    * @return null
+    */
+    protected function setPoints($value)
+    {
+        if (is_array($value)) {
+            $this->data["points"] = json_encode($value);
+        } else if (is_string($value)) {
+            $this->data["points"] = $value;
+        }
+    }
+}
+?>
