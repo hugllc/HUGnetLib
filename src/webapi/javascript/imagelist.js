0775ed77fc71fba883154bb93432e997a8f0ad5d
Scott Price <prices@hugllc.com>
2014-03-20 21:27:59 -0500

    More work on bug #8138.  The first shot at the image viewer.

---
+++ src/webapi/javascript/imagelist.js
@@ -0,0 +1,140 @@
+/**
+ * hugnet.image.js
+ *
+ * <pre>
+ * HUGnetLib is a user interface for the HUGnet
+ * Copyright (C) 2012 Hunt Utilities Group, LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   JavaScript
+ * @package    HUGnetLib
+ * @subpackage Images
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2012 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+/**
+* This is the model that stores the images.
+*
+* @category   JavaScript
+* @package    HUGnetLib
+* @subpackage Tests
+* @author     Scott Price <prices@hugllc.com>
+* @copyright  2012 Hunt Utilities Group, LLC
+* @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+* @version    Release: 0.9.7
+* @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+*/
+HUGnet.ImageList = Backbone.View.extend({
+    tabs: undefined,
+    id: "images-tabs",
+    readonly: false,
+    data: {},
+    filter: {publish: 1},
+    url: '/HUGnetLib/HUGnetLibAPI.php',
+    tabTemplate: '<li style="white-space: nowrap;"><a href="#{href}">#{label}</a> <span name="#{href}" class="ui-icon ui-icon-close">Remove Tab</span></li>',
+    initialize: function (options)
+    {
+        if (options) {
+            if (options.url) {
+                this.url = options.url;
+            }
+            if (options.id) {
+                this.id = options.id;
+            }
+            if (options.readonly) {
+                this.readonly = options.readonly;
+            }
+        }
+        this.images = new HUGnet.ImageListView({
+            model: options.images,
+            url: this.url,
+        });
+        this.render();
+    },
+    render: function ()
+    {
+        this.$el.html('<div id="'+this.id+'"><ul></ul></div>');
+        var self = this;
+        this.tabs = $('#'+this.id).tabs({
+            active: 0,
+            cookie: {
+                // store a session cookie
+                expires: 10
+            },
+            activate: function(event, ui) 
+            {
+                self.$(".tablesorter").trigger("update");
+                var tag = ui.newPanel.selector;
+                tag = tag.replace("#", "");
+                if (self.data[tag]) {
+                    self.data[tag].trigger("update");
+                }
+            }
+        });
+        var tag = this.id+'-views';
+        this.tabs.find( ".ui-tabs-nav" ).append('<li><a href="#'+tag+'">Image List</a></li>');
+        this.tabs.append( '<div id="'+tag+'"></div>' );
+        $('#'+tag).html(this.images.render().el);
+        this.tabs.tabs("refresh");
+        this.tabs.tabs("option", "active", 0);
+
+        /* Further tabs will have a close button */
+        //this.tabs.tabs("option", "tabTemplate", '<li style="white-space: nowrap;"><a href="#{href}">#{label}</a> <span name="#{href}" class="ui-icon ui-icon-close">Remove Tab</span></li>');
+        /* close icon: removing the tab on click */
+        var tabs = this.tabs;
+        $(document).on( "click", "#"+this.id+" span.ui-icon-close", function(event, ui) {
+            var panelId = $( this ).closest( "li" ).remove().attr( "aria-controls" );
+            self.data[panelId].exit();
+            delete self.data[panelId];
+            $( "#" + panelId ).remove();
+            tabs.tabs( "refresh" );
+        });
+
+        this.images.bind(
+            "view",
+            function (image)
+            {
+                this.imageTab(image);
+            },
+            this
+        );
+    },
+    imageTab: function (image)
+    {
+        var self = this;
+        var tag = this.id + image.get("id");
+        if (this.data[tag] !== undefined) {
+            return;
+        }
+        this.data[tag] = new HUGnet.ImageView({
+            parent: tag,
+            model: image,
+            TestID: 1,
+            url: this.url
+        });
+        var title = 'Image "' + image.get("ImageID") + '" History';
+
+        //this.tabs.tabs("add", tag, title);
+        this.tabs.find( ".ui-tabs-nav" ).append('<li><a href="#'+tag+'">'+title+'</a> <span name="#{href}" class="ui-icon ui-icon-close">Remove Tab</span></li>');
+        this.tabs.append( "<div id='"+tag+"'></div>" );
+        $("#"+tag).html(this.data[tag].render().el);
+        this.tabs.tabs("refresh");
+        this.tabs.tabs("option", "active", -1);
+    },
+});
---
+++ src/webapi/javascript/views/hugnet.imagelist.js
@@ -0,0 +1,165 @@
+/**
+ * hugnet.device.js
+ *
+ * <pre>
+ * HUGnetLib is a user interface for the HUGnet
+ * Copyright (C) 2012 Hunt Utilities Group, LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   JavaScript
+ * @package    HUGnetLib
+ * @subpackage Tests
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2012 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+/**
+* This is the model that stores the devices.
+*
+* @category   JavaScript
+* @package    HUGnetLib
+* @subpackage Tests
+* @author     Scott Price <prices@hugllc.com>
+* @copyright  2012 Hunt Utilities Group, LLC
+* @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+* @version    Release: 0.9.7
+* @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+*/
+var ImageListEntryView = Backbone.View.extend({
+    model: HUGnet.Device,
+    tagName: 'tr',
+    template: '#ImageListViewEntryTemplate',
+    parent: null,
+    events: {
+        'click .view': 'view',
+        'click .export': 'export'
+    },
+    initialize: function (options)
+    {
+        if (options) {
+            if (options.url) {
+                this.url = options.url;
+            }
+            if (options.templatebase) {
+                this.template = '#' + options.templatebase + 'EntryTemplate';
+            }
+        }
+        this.model.on('change', this.render, this);
+        this.model.on('remove', this.remove, this);
+        this.parent = options.parent;
+    },
+    view: function (e)
+    {
+        this.parent.trigger("view", this.model);
+    },
+    export: function (e)
+    {
+        this.parent.trigger("export", this.model);
+    },
+    /**
+    * Gets infomration about a device.  This is retrieved directly from the device
+    *
+    * This function is for use of the device list
+    *
+    * @param id The id of the device to get
+    *
+    * @return null
+    */
+    render: function ()
+    {
+        var data = this.model.toJSON();
+        _.extend(data, HUGnet.viewHelpers);
+        this.$el.html(
+            _.template(
+                $(this.template).html(),
+                data
+            )
+        );
+        this.$el.trigger('update');
+        return this;
+    }
+});
+
+/**
+* This is the model that stores the devices.
+*
+* @category   JavaScript
+* @package    HUGnetLib
+* @subpackage Tests
+* @author     Scott Price <prices@hugllc.com>
+* @copyright  2012 Hunt Utilities Group, LLC
+* @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+* @version    Release: 0.9.7
+* @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+*/
+HUGnet.ImageListView = Backbone.View.extend({
+    template: "#ImageListViewTemplate",
+    templatebase: 'ImageListView',
+    url: '/HUGnetLib/HUGnetLibAPI.php',
+    readonly: false,
+    views: {},
+    sorted: false,
+    sorting: [[1,0]],
+    viewed: 0,
+    events: {
+    },
+    initialize: function (options)
+    {
+        if (options) {
+            if (options.url) {
+                this.url = options.url;
+            }
+        }
+        this.model.on('add', this.insert, this);
+    },
+    /**
+    * Gets infomration about a device.  This is retrieved directly from the device
+    *
+    * This function is for use of the device list
+    *
+    * @return null
+    */
+    render: function ()
+    {
+        var data = this.model.toJSON();
+        _.extend(data, HUGnet.viewHelpers);
+        this.$el.html(
+            _.template(
+                $(this.template).html(),
+                data
+            )
+        );
+        this.$('table').tablesorter({
+            widgets: ['zebra'],
+            widgetOptions: { zebra: [ 'even', 'odd' ] }
+        });
+        this.$el.trigger('update');
+        this.trigger("update");
+        return this;
+    },
+    insert: function (model, collection, options)
+    {
+        var view = new ImageListEntryView({
+            model: model,
+            parent: this,
+            templatebase: this.templatebase
+        });
+        this.$('tbody').append(view.render().el);
+        this.$('table').trigger('update');
+    },
+});
