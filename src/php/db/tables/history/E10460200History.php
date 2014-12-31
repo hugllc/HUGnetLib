9a623184371bbedfb0570b0f227e438694d44a08
Scott Price <prices@hugllc.com>
2014-12-31 12:21:43 -0600

    This fixes #8440.

---
+++ src/php/db/tables/history/E10460200History.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * This is the default endpoint driver and the base for all other
+ * endpoint drivers.
+ *
+ * PHP Version 5
+ * <pre>
+ * HUGnetLib is a library of HUGnet code
+ * Copyright (C) 2014 Hunt Utilities Group, LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   Libraries
+ * @package    HUGnetLib
+ * @subpackage PluginsAverageTable
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2014 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link       http://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+namespace HUGnet\db\tables;
+/** This keeps this file from being included unless HUGnetSystem.php is included */
+defined('_HUGNET') or die('HUGnetSystem not found');
+/** This is our system interface */
+require_once dirname(__FILE__)."/../../../interfaces/DBTable.php";
+/** This is our system interface */
+require_once dirname(__FILE__)."/../../../interfaces/DBTableHistory.php";
+
+/**
+ * This class has functions that relate to the manipulation of elements
+ * of the devInfo array.
+ *
+ * @category   Libraries
+ * @package    HUGnetLib
+ * @subpackage PluginsAverageTable
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2014 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    Release: 0.14.3
+ * @link       http://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+class E10460200History extends \HUGnet\db\History
+    implements \HUGnet\interfaces\DBTable, \HUGnet\interfaces\DBTableHistory
+{
+    /** @var string This is the table we should use */
+    public $sqlTable = "e10460200_history";
+    /** @var This is the dataset */
+    public $datacols = 50;
+
+    /******************************************************************
+     ******************************************************************
+     ********  The following are input modification functions  ********
+     ******************************************************************
+     ******************************************************************/
+
+}
+?>
