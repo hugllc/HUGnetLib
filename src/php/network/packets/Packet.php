0c2568868c1c6bc630abb7030bc15b74128db291
Scott Price <prices@hugllc.com>
2015-04-25 09:11:24 -0500

    Added a packet name for bug #8511.

--- src/php/network/packets/Packet.php
+++ src/php/network/packets/Packet.php
@@ -134,6 +134,7 @@ final class Packet implements PacketInterface
         "WRITE_E2" => 0x1A,
         "WRITE_SRAM" => 0x1B,
         "WRITE_FLASH" => 0x1C,
+        "FRIEND" => 0x21,
         "PORT_ON" => 0x30,
         "PORT_OFF" => 0x31,
         "SETPOWERTABLE" => 0x45,
