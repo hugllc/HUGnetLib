821ef0b0e5f7516b9f072a1377d05b5785b60535
Jeff Liesmaki <jeffl@hugllc.com>
2015-11-13 14:53:55 -0600

    add table to log device test id and test results  bug #8474

---
+++ src/php/db/tables/DeviceTests.php
@@ -0,0 +1,184 @@
+<?php
+/**
+ * This is the default endpoint driver and the base for all other
+ * endpoint drivers.
+ *
+ * PHP Version 5
+ * <pre>
+ * HUGnetLib is a library of HUGnet code
+ * Copyright (C) 2014 Hunt Utilities Group, LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   Libraries
+ * @package    HUGnetLib
+ * @subpackage Tables
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2014 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link       http://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+namespace HUGnet\db\tables;
+/** This keeps this file from being included unless HUGnetSystem.php is included */
+defined('_HUGNET') or die('HUGnetSystem not found');
+/** This is our system interface */
+require_once dirname(__FILE__)."/../../interfaces/DBTable.php";
+
+/**
+ * This class has functions that relate to the manipulation of elements
+ * of the devInfo array.
+ *
+ * @category   Libraries
+ * @package    HUGnetLib
+ * @subpackage Tables
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2014 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version    Release: 0.14.3
+ * @link       http://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+class DeviceTests extends \HUGnet\db\Table
+    implements \HUGnet\interfaces\DBTable
+{
+    /** @var string This is the table we should use */
+    public $sqlTable = "devicetests";
+    /** @var string This is the primary key of the table.  Leave blank if none  */
+    public $sqlId = "id";
+    /**
+    * @var array This is the definition of the columns
+    *
+    * This should consist of the following structure:
+    * array(
+    *   "name" => array(
+    *       "Name"          => string The name of the column
+    *       "Type"          => string The type of the column
+    *       "Default"       => mixed  The default value for the column
+    *       "Null"          => bool   true if null is allowed, false otherwise
+    *       "AutoIncrement" => bool   true if the column is auto_increment
+    *       "CharSet"       => string the character set if the column is text or char
+    *       "Collate"       => string colation if the table is text or char
+    *       "Primary"       => bool   If we are a primary Key.
+    *       "Unique"        => bool   If we are a unique column.
+    *   ),
+    *   "name2" => array(
+    *   .
+    *   .
+    *   .
+    * );
+    *
+    * Not all fields have to be filled in.  Name and Type are the only required
+    * fields.  The index of the base array should be the same as the "Name" field.
+    */
+    public $sqlColumns = array(
+        "id" => array(
+            "Name" => "id",
+            "Type" => "int",
+            "Primary" => true,
+        ),
+        "DeviceID" => array(
+            "Name" => "DeviceID",
+            "Type" => "varchar(6)",
+            "Default" => '000000',
+        ),
+        "DeviceName" => array(
+            "Name" => "DeviceName",
+            "Type" => "varchar(128)",
+            "Default" => '',
+        ),
+        "HWPartNum" => array(
+            "Name" => "HWPartNum",
+            "Type" => "varchar(12)",
+            "Default" => '',
+        ),
+        "MicroSN" => array(
+            "Name" => "MicroSN",
+            "Type" => "varchar(22)",
+            "Default" => '',
+        ),
+        "TestDate" => array(
+            "Name" => "TestDate",
+            "Type" => "varchar(10)",
+            "Default" => "",
+        ),
+        "TestResult" => array(
+            "Name" => "TestResult",
+            "Type" => "varchar(4)",
+            "Default" => "FAIL",
+        ),
+        "TestsFailed" => array(
+            "Name" => "TestsFailed",
+            "Type" => "longtext",
+            "Default" => "None",
+        ),
+        "RetestTimes" => array(
+            "Name" => "RetestTimes",
+            "Type" => "tinyint(4)",
+            "Default" => 0,
+        ),
+    );
+    /**
+    * @var array This is the definition of the indexes
+    *
+    *   array(
+    *       "Name" => array (
+    *           "Name"    => string The name of the index
+    *           "Unique"  => bool   Create a Unique index
+    *           "Columns" => array  Array of column names
+    *       ),
+    *       "name2" => array(
+    *       .
+    *       .
+    *   ),
+    */
+    public $sqlIndexes = array(
+        "id" => array(
+            "Name" => "id",
+            "Unique" => true,
+            "Columns" => array("id"),
+        ),
+        "name" => array(
+            "Name" => "name",
+            "Unique" => true,
+            "Columns" => array("name"),
+        ),
+    );
+
+    /** @var array This is the default values for the data */
+    protected $default = array(
+        "group" => "default",    // Server group to use
+    );
+    /**
+    * This is the constructor
+    *
+    * @param object &$system  The system object to use
+    * @param mixed  $data     This is an array or string to create the object from
+    * @param object &$connect The connection manager
+    */
+    protected function __construct(&$system, $data="", &$connect = null)
+    {
+        parent::__construct($system, $data, $connect);
+        $this->create();
+    }
+
+    /******************************************************************
+     ******************************************************************
+     ********  The following are input modification functions  ********
+     ******************************************************************
+     ******************************************************************/
+
+}
+?>
