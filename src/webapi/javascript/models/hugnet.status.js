b3a66ce32e6f5dab32d60bb5d6f3640b7735ad4f
Scott L. Price <prices@hugllc.com>
2016-01-25 09:40:50 -0600

    Work on bug #8528.  This file needed to be added.

---
+++ src/webapi/javascript/models/hugnet.status.js
@@ -0,0 +1,88 @@
+/**
+ * hugnet.device.js
+ *
+ * <pre>
+ * HUGnetLib is a user interface for the HUGnet
+ * Copyright (C) 2014 Hunt Utilities Group, LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * </pre>
+ *
+ * @category   JavaScript
+ * @package    HUGnetLib
+ * @subpackage Devices
+ * @author     Scott Price <prices@hugllc.com>
+ * @copyright  2016 Hunt Utilities Group, LLC
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+ */
+/**
+* This is the model that stores the devices.
+*
+* @category   JavaScript
+* @package    HUGnetLib
+* @subpackage Devices
+* @author     Scott Price <prices@hugllc.com>
+* @copyright  2016 Hunt Utilities Group, LLC
+* @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+* @version    Release: 0.14.3
+* @link       https://dev.hugllc.com/index.php/Project:HUGnetLib
+*/
+HUGnet.Status = Backbone.Model.extend({
+    idAttribute: 'id',
+    urlPart: '/status',
+    baseurl: '',
+    refresh: 10,
+    timer: null,
+    defaults:
+    {
+        id: null,
+        devicesTotal: 0,
+        devicesActive: 0,
+        HWPartNum: null,
+        FWPartNum: null,
+        FWVersion: null,
+        systemType: "UNKNOWN",
+        Current: 0,
+        Voltage: 0,
+        Temperature: 0,
+        Charge: 0,
+        Capacity:0,
+        Status: 0,
+        StatusMSG: "Power Up",
+        CoachDevices: 0,
+        Ports: 0
+    },
+    /**
+    * This function initializes the object
+    */
+    initialize: function(options)
+    {
+        if (options) {
+            if (options.baseurl) this.baseurl = options.baseurl;
+        }
+    },
+    /** This gives us our URL */
+    url: function ()
+    {
+        return this.baseurl + this.urlPart;
+    },
+    /**
+    * This function initializes the object
+    */
+    fix: function(attributes)
+    {
+    },
+});
